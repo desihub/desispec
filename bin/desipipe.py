0a08e689ed068757691a61f6a514c3c2a67996f3
Theodore Kisner <tskisner.public@gmail.com>
2014-11-03 15:22:27 -0800

    small cleanups

---
+++ bin/desipipe.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+"""
+This script generates example bash scripts containing pipeline commands.
+"""
+#
+
+import sys
+
+import desispec.pipeline as dp
+
+
--- py/desispec/__init__.py
+++ py/desispec/__init__.py
@@ -2,14 +2,10 @@
 # -*- coding: utf-8 -*-
 """
 ========
-template
+desispec
 ========
 
-This package is a template for other DESI_ Python_ packages.
-
-The function desiUtil.install.version() should be used to set the ``__version__``
-package variable.  In order for this to work properly, the svn property
-svn:keywords must be set to HeadURL on this file.
+Tools for DESI spectroscopic processing.
 
 .. _DESI: http://desi.lbl.gov
 .. _Python: http://python.org
@@ -17,12 +13,5 @@ svn:keywords must be set to HeadURL on this file.
 #
 from __future__ import absolute_import, division, print_function, unicode_literals
 # The line above will help with 2to3 support.
-from desiUtil.install import version
-#
-# Set version string.
-#
-__version__ = version('$HeadURL: https://desi.lbl.gov/svn/code/tools/desiTemplate/trunk/py/desiTemplate/__init__.py $')
-#
-# Clean up namespace
-#
-del version
+
+
