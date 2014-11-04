fe1afa070061a63535e323c9e60ff04131937224
Theodore Kisner <tskisner.public@gmail.com>
2014-11-04 12:05:23 -0800

    switch to spaces for indentation

--- py/desispec/pipeline/core.py
+++ py/desispec/pipeline/core.py
@@ -5,15 +5,15 @@
 
 
 class Module( object ):
-	"""
-	This class represents a step of the pipeline.  Usually it represents a
-	single small part of the processing, but one could create Modules that
-	perform several steps simultaneously.
-	"""
+    """
+    This class represents a step of the pipeline.  Usually it represents a
+    single small part of the processing, but one could create Modules that
+    perform several steps simultaneously.
+    """
 
-	def __init__( self ):
+    def __init__( self ):
 
-	def parallelism( self ):
-		raise NotImplementedError('Pipeline Module parallelism() method not implemented')
+    def parallelism( self ):
+        raise NotImplementedError('Pipeline Module parallelism() method not implemented')
 
 
--- py/desispec/pipeline/run.py
+++ py/desispec/pipeline/run.py
@@ -5,22 +5,27 @@
 
 
 class Machine( object ):
-	"""
-	This class represents the properties of a single machine.  This includes
-	the node configuration, etc.
-	"""
+    """
+    This class represents the properties of a single machine.  This includes
+    the node configuration, etc.
+    """
 
-	def __init__( self ):
+    def __init__( self ):
 
-	def nodes( self ):
-		"""
-		Returns the maximum number of nodes to use on this machine.
-		"""
-		return 1
+    def nodes( self ):
+        """
+        Returns the maximum number of nodes to use on this machine.
+        """
+        return 1
 
-	def cores_per_node( self ):
-		"""
-		Returns the number of cores per node on this machine.
-		"""
-		return 1
+    def cores_per_node( self ):
+        """
+        Returns the number of cores per node on this machine.
+        """
+        return 1
+
+
+
+
+class MachineEdison
 
