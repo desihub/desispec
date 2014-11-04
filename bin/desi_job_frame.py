8822cc3170fd5693cce0ad2cbe8b0a73be986bee
Theodore Kisner <tskisner.public@gmail.com>
2014-11-04 11:01:28 -0800

    rename test script

---
+++ bin/desi_job_frame.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+This script generates a bash script which contains all commands needed to process a single DESI frame.
+"""
+
+
+import sys
+
+import desispec.pipeline as dp
+
+
