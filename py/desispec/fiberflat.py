2bed21dbcd766c88d2bf10c6d67fd0e22e5a0e33
Julien Guy <jguy@lbl.gov>
2015-01-27 16:43:26 -0800

    first version of fiberflat

---
+++ py/desispec/fiberflat.py
@@ -0,0 +1,166 @@
+"""
+Utility functions to compute a fiber flat correction and apply it
+We try to keep all the (fits) io separated.
+"""
+
+
+import numpy as np
+from desispec.io.frame import resolution_data_to_sparse_matrix
+from desispec.linalg import cholesky_solve
+from desispec.linalg import cholesky_solve_and_invert
+from desispec.linalg import spline_fit
+import scipy,scipy.sparse
+import sys
+
+def compute_fiberflat(wave,flux,ivar,resolution_data,nsig_clipping=4.) :
+    
+    """ 
+    compute fiber flat by deriving an average spectrum and dividing all fiber data by this average.
+    input data are expected to be on the same wavelenght grid, with uncorrelated noise.
+    they however do not have exactly the same resolution.
+
+    we first iteratively :
+       - compute a deconvolved mean spectrum
+       - compute a fiber flat using the resolution convolved mean spectrum for each fiber
+       - smooth the fiber flat along wavelength 
+       - clip outliers
+
+    and then we compute an fiberflat at the native fiber resolution (not smoothed)
+    
+    the routine returns the fiberflat, its inverse variance , and the deconvolved mean spectrum
+
+    """
+
+    # chi2 = sum_(fiber f) sum_(wavelenght i) w_fi ( D_fi - F_fi (R_f M)_i )
+    # A_kl = sum_(fiber f) sum_(wavelenght i) w_fi F_fi^2 (R_fki R_fli)
+    # B_k = sum_(fiber f) sum_(wavelenght i) w_fi D_fi F_fi R_fki
+    
+    # A = sum_(fiber f) sum_(wavelenght i) w_fi F_fi^2 (R_fi R_fi^T)
+    # B = sum_(fiber f) sum_(wavelenght i) w_fi D_fi F_fi R_fi
+    
+    
+    
+    nwave=wave.size
+    nfibers=flux.shape[0]
+    
+    
+
+    # iterative fitting and clipping to get precise mean spectrum
+    current_ivar=ivar.copy()
+    
+    
+    smooth_fiberflat=np.ones((flux.shape))
+    chi2=np.zeros((flux.shape))
+    
+
+    sqrtwflat=np.sqrt(current_ivar)*smooth_fiberflat
+    sqrtwflux=np.sqrt(current_ivar)*flux
+
+
+    # test
+    #nfibers=20
+    nout_tot=0
+    for iteration in range(20) :
+
+        # fit mean spectrum
+        A=scipy.sparse.lil_matrix((nwave,nwave)).tocsr()
+        B=np.zeros((nwave))
+        
+        # diagonal sparse matrix with content = sqrt(ivar)*flat of a given fiber
+        SD=scipy.sparse.lil_matrix((nwave,nwave))
+        
+        # loop on fiber to handle resolution
+        for fiber in range(nfibers) :
+            if fiber%10==0 :
+                print "fiber",fiber
+            R = resolution_data_to_sparse_matrix(resolution_data,fiber)
+            
+            # diagonal sparse matrix with content = sqrt(ivar)*flat
+            SD.setdiag(sqrtwflat[fiber])
+            
+            sqrtwflatR = R*SD # each col c of R is multiplied by sqrtwflat[c]
+                       
+            A = A+(sqrtwflatR*sqrtwflatR.T).tocsr()
+            B += sqrtwflatR*sqrtwflux[fiber]
+        
+        print "solving"
+        mean_spectrum=cholesky_solve(A.todense(),B)
+        
+        print "smoothing"
+        # fit smooth fiberflat and compute chi2
+        smoothing_res=100. #A
+        
+        for fiber in range(nfibers) :
+            if fiber%10==0 :
+                print "fiber",fiber
+            R = resolution_data_to_sparse_matrix(resolution_data,fiber)
+            
+            #M = np.array(np.dot(R.todense(),mean_spectrum)).flatten()
+            M = R.dot(mean_spectrum)
+                        
+            F = flux[fiber]/(M+(M==0))
+            smooth_fiberflat[fiber]=spline_fit(wave,wave,F,smoothing_res,current_ivar[fiber]*(M!=0))
+            chi2[fiber]=current_ivar[fiber]*(flux[fiber]-smooth_fiberflat[fiber]*M)**2
+        
+        print "rejecting"
+        
+        nout_iter=0
+        if iteration<1 :
+            # only remove worst outlier per wave
+            # apply rejection iteratively, only one entry per wave among fibers
+            # find waves with outlier (fastest way)
+            nout_per_wave=np.sum((chi2>0)*(chi2>nsig_clipping**2),axis=0)
+            selection=np.where(nout_per_wave>0)[0]
+            for i in selection :
+                worst_entry=np.argmax(chi2[:,i])
+                current_ivar[worst_entry,i]=0
+                sqrtwflat[worst_entry,i]=0
+                sqrtwflux[worst_entry,i]=0
+                nout_iter += 1
+                
+        else :
+            # remove all of them at once
+            bad=(chi2>nsig_clipping**2)
+            current_ivar *= (bad==0)
+            sqrtwflat *= (bad==0)
+            sqrtwflux *= (bad==0)
+            nout_iter += np.sum(bad)
+        
+        nout_tot += nout_iter
+
+        sum_chi2=float(np.sum(chi2))
+        ndf=int(np.sum(chi2>0)-nwave-nfibers*(nwave/smoothing_res))
+        chi2pdf=0.
+        if ndf>0 :
+            chi2pdf=sum_chi2/ndf
+        print "iter #%d chi2=%f ndf=%d chi2pdf=%f nout=%d"%(iteration,sum_chi2,ndf,chi2pdf,nout_iter)
+
+        # normalize to get a mean fiberflat=1
+        mean=np.mean(smooth_fiberflat)
+        smooth_fiberflat = smooth_fiberflat/mean
+        mean_spectrum    = mean_spectrum*mean
+        
+
+
+        if nout_iter == 0 :
+            break
+    
+    print "nout tot=",nout_tot
+
+    # now use mean spectrum to compute flat field correction without any smoothing
+    # because sharp feature can arise if dead columns
+    
+    fiberflat=np.ones((flux.shape))
+    fiberflat_ivar=np.zeros((flux.shape))
+    
+    for fiber in range(nfibers) :
+        R = resolution_data_to_sparse_matrix(resolution_data,fiber)
+        M = np.array(np.dot(R.todense(),mean_spectrum)).flatten()
+        fiberflat[fiber] = (M!=0)*flux[fiber]/(M+(M==0)) + (M==0)
+        fiberflat_ivar[fiber] = ivar[fiber]*M**2
+
+    return fiberflat,fiberflat_ivar,mean_spectrum
+
+    
+
+
---
+++ py/desispec/linalg.py
@@ -0,0 +1,62 @@
+import numpy as np
+import scipy,scipy.linalg,scipy.interpolate
+
+
+def cholesky_solve(A,B,overwrite=False,lower=False) :
+    posv, = scipy.linalg.get_lapack_funcs(('posv',), (A,))
+    L,X,status=posv(A,B,lower=lower,overwrite_a=overwrite)
+    
+    if status :
+        print "error cholesky_solve_and_invert error dposv status=",status
+        raise Exception("cholesky_solve_and_invert error dposv status=%d"%status)
+    
+    return X
+
+def cholesky_solve_and_invert(A,B,overwrite=False,lower=False) :
+    posv, = scipy.linalg.get_lapack_funcs(('posv',), (A,))
+    L,X,status=posv(A,B,lower=lower,overwrite_a=overwrite)
+    
+    if status :
+        print "error cholesky_solve_and_invert error dposv status=",status
+        raise Exception("cholesky_solve_and_invert error dposv status=%d"%status)
+    
+    potri, = scipy.linalg.get_lapack_funcs(('potri',), (L,))
+    inv,status=potri(L,lower=(not lower)) # 'not lower' is not a mistake, there is a BUG in scipy!!!!   
+        
+    if status :
+        print "error cholesky_solve_and_invert error dpotri status=",status
+        raise Exception("cholesky_solve_and_invert error dpotri status=%d"%status)
+
+    #symmetrize Ai
+    if True :
+        for i in range(inv.shape[0]) :
+            for j in range(i) :
+                if  not lower :
+                    inv[i,j]=inv[j,i]
+                else :
+                    inv[j,i]=inv[i,j]
+    return X,inv
+
+
+def spline_fit(output_wave,input_wave,input_flux,required_resolution,input_ivar=None,order=3) :
+    """
+    performs a spline fit 
+    """
+    if input_ivar != None :
+        selection=np.where(input_ivar>0)[0]
+        if selection.size < 2 :
+            print "cannot do spline fit because only",selection.size,"values with ivar>0"
+            raise Error 
+        w1=input_wave[selection[0]]
+        w2=input_wave[selection[-1]]
+    else :
+        w1=input_wave[0]
+        w2=input_wave[-1]
+
+    res=required_resolution
+    n=int((w2-w1)/res)
+    res=(w2-w1)/(n+1)
+    knots=w1+res*(0.5+np.arange(n))
+    toto=scipy.interpolate.splrep(input_wave,input_flux,w=input_ivar,k=order,task=-1,t=knots)
+    output_flux = scipy.interpolate.splev(output_wave,toto)
+    return output_flux
