aef70f09a7eb3135080af5a377f1838ea97473c6
David Kirkby <dkirkby@uci.edu>
2015-03-03 11:48:49 -0800

    Update brick maker

--- bin/desi_make_bricks.py
+++ bin/desi_make_bricks.py
@@ -15,8 +15,10 @@ def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--verbose', action = 'store_true',
         help = 'Provide verbose reporting of progress.')
-    parser.add_argument('--night', default = None, metavar = 'YYYYMMDD',
+    parser.add_argument('--night', type = str, default = None, metavar = 'YYYYMMDD',
         help = 'Night to process in the format YYYYMMDD')
+    parser.add_argument('--specprod', type = str, default = None, metavar = 'PATH',
+        help = 'Override default path ($DESI_SPECTRO_REDUX/$PRODNAME) to processed data.')
     args = parser.parse_args()
 
     if args.night is None:
@@ -25,23 +27,24 @@ def main():
 
     try:
         # Loop over exposures available for this night.
-        for exposure in desispec.io.get_exposures(args.night):
+        for exposure in desispec.io.get_exposures(args.night,specprod = args.specprod):
             # Ignore exposures with no fibermap, assuming they are calibration data.
-            fibermap_path = desispec.io.findfile(filetype = 'fibermap', night = args.night,
-                expid = exposure)
-            print fibermap_path
+            fibermap_path = desispec.io.findfile(filetype = 'fibermap',night = args.night,
+                expid = exposure,specprod = args.specprod)
             if not os.path.exists(fibermap_path):
                 if args.verbose:
-                    print 'Skipping exposure %d with no fibermap.' % exposure
+                    print 'Skipping exposure %08d with no fibermap.' % exposure
                 continue
             # Open the fibermap.
             fibermap_data,fibermap_hdr = desispec.io.read_fibermap(fibermap_path)
-            print fibermap_data.dtype
-            # Look for cframes associated with this exposure.
-            cframe_path = desispec.io.findfile(filetype = 'cframe',night = args.night,
-                expid = exposure, camera = '*')
-            for entry in glob.glob(cframe_path):
-                print entry
+            # Get the set of bricknames used in this fibermap.
+            bricknames = set(fibermap_data['BRICKNAME'])
+            # Get the list of per-camera cframes available for this exposure.
+            cframes = desispec.io.get_files(filetype = 'cframe',night = args.night,
+                expid = exposure,specprod = args.specprod)
+            if args.verbose:
+                print 'Exposure %08d: %d bricks, cframes: %s' % (
+                    exposure,len(bricknames),' '.join(cframes.keys()))
 
     except RuntimeError,e:
         print str(e)
