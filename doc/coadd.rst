5c5bbf8294d6ea2f3d777b5eeb2f2583969ac219
David Kirkby <dkirkby@uci.edu>
2015-03-06 13:48:44 -0800

    Convert tabs to spaces

--- doc/coadd.rst
+++ doc/coadd.rst
@@ -6,52 +6,52 @@ DESI Environment
 
 Ssh to edison.nersc.gov (remember to use `ssh -A` to propagate your keys for github access) and::
 
-	source /project/projectdirs/desi/software/modules/desi_environment.sh
+    source /project/projectdirs/desi/software/modules/desi_environment.sh
 
 Installation
 ------------
 
 Clone the git package and select the co-add development branch::
 
-	git clone git@github.com:desihub/desispec.git
-	cd desispec
-	git checkout \#3
+    git clone git@github.com:desihub/desispec.git
+    cd desispec
+    git checkout \#3
 
 Per-Login Setup
 ---------------
 
 Manually set paths for using this installation (assuming `bash`)::
 
-	cd desispec
-	export PATH=$PWD/bin:$PATH
-	export PYTHONPATH=$PWD/py:$PYTHONPATH
+    cd desispec
+    export PATH=$PWD/bin:$PATH
+    export PYTHONPATH=$PWD/py:$PYTHONPATH
 
 Set pipeline paths::
 
-	export DESI_SPECTRO_REDUX=$DESI_ROOT/spectro/redux
-	export PRODNAME=sjb/cedar2a
-	export DESI_SPECTRO_SIM=$DESI_ROOT/spectro/sim
-	export DESI_SPECTRO_DATA=$DESI_SPECTRO_SIM/alpha-5
+    export DESI_SPECTRO_REDUX=$DESI_ROOT/spectro/redux
+    export PRODNAME=sjb/cedar2a
+    export DESI_SPECTRO_SIM=$DESI_ROOT/spectro/sim
+    export DESI_SPECTRO_DATA=$DESI_SPECTRO_SIM/alpha-5
 
 Tests
 -----
 
 Convert mocks cframes and fibermaps into brick files using::
 
-	rm -rf $DESI_SPECTRO_REDUX/$PRODNAME/bricks
-	desi_make_bricks.py --night 20150211 --verbose
+    rm -rf $DESI_SPECTRO_REDUX/$PRODNAME/bricks
+    desi_make_bricks.py --night 20150211 --verbose
 
 Note that the code is not yet smart enough to do the right thing for exposures that have already been added to brick files, hence the `rm` command above.
 
 Inspect a brick file in iPython using, e.g.::
 
-	import os,os.path
-	import astropy.io.fits as fits
-	from astropy.table import Table
-	brick = fits.open(os.path.join(os.getenv('DESI_SPECTRO_REDUX'),os.getenv('PRODNAME'),'bricks','3582m005','brick-r-3582m005.fits'))
-	info = Table.read(brick,hdu=4)
-	print info
-	plt.errorbar(x=brick[2].data,y=brick[0].data[0],yerr=brick[1].data[0]**-0.5)
+    import os,os.path
+    import astropy.io.fits as fits
+    from astropy.table import Table
+    brick = fits.open(os.path.join(os.getenv('DESI_SPECTRO_REDUX'),os.getenv('PRODNAME'),'bricks','3582m005','brick-r-3582m005.fits'))
+    info = Table.read(brick,hdu=4)
+    print info
+    plt.errorbar(x=brick[2].data,y=brick[0].data[0],yerr=brick[1].data[0]**-0.5)
 
 Notes
 -----
--- py/desispec/io/brick.py
+++ py/desispec/io/brick.py
@@ -14,132 +14,132 @@ import astropy.io.fits
 import desispec.io.util
 
 class Brick(object):
-	"""
-	Represents objects in a single band (b,r,z) and brick.
+    """
+    Represents objects in a single band (b,r,z) and brick.
 
-	The constructor will open an existing file and create a new file and parent
-	directory if necessary.  The :meth:`close` method must be called for any updates
-	or new data to be recorded. Successful completion of the constructor does not
-	guarantee that :meth:`close` will suceed.
+    The constructor will open an existing file and create a new file and parent
+    directory if necessary.  The :meth:`close` method must be called for any updates
+    or new data to be recorded. Successful completion of the constructor does not
+    guarantee that :meth:`close` will suceed.
 
-	Args:
-		path(str): Path to the brick file to open.
-		mode(str): File access mode to use. Should normally be 'readonly' or 'update'.
-			Use 'update' to create a new file and its parent directory if necessary.
-		header: An optional header specification used to create a new file. See
-			:func:`desispec.io.util.fitsheader` for details on allowed values.
+    Args:
+        path(str): Path to the brick file to open.
+        mode(str): File access mode to use. Should normally be 'readonly' or 'update'.
+            Use 'update' to create a new file and its parent directory if necessary.
+        header: An optional header specification used to create a new file. See
+            :func:`desispec.io.util.fitsheader` for details on allowed values.
 
-	Raises:
-		RuntimeError: Invalid mode requested.
-		IOError: Unable to open existing file in 'readonly' mode.
-		OSError: Unable to create a new parent directory in 'update' mode.
-	"""
-	def __init__(self,path,mode = 'readonly',header = None):
-		if mode not in ('readonly','update'):
-			raise RuntimeError('Invalid mode %r' % mode)
-		self.path = path
-		self.mode = mode
-		# Create a new file if necessary.
-		if self.mode == 'update' and not os.path.exists(self.path):
-			# Create the parent directory, if necessary.
-			head,tail = os.path.split(self.path)
-			if not os.path.exists(head):
-				os.makedirs(head)
-			# Create empty HDUs. It would be good to refactor io.frame to avoid any duplication here.
-			hdr = desispec.io.util.fitsheader(header)
-			hdr['EXTNAME'] = ('FLUX', 'no dimension')
-			hdu0 = astropy.io.fits.PrimaryHDU(header = hdr)
-			hdr['EXTNAME'] = ('IVAR', 'no dimension')
-			hdu1 = astropy.io.fits.ImageHDU(header = hdr)
-			hdr['EXTNAME'] = ('WAVELENGTH', '[Angstroms]')
-			hdu2 = astropy.io.fits.ImageHDU(header = hdr)
-			hdr['EXTNAME'] = ('RESOLUTION', 'no dimension')
-			hdu3 = astropy.io.fits.ImageHDU(header = hdr)
-			hdr['EXTNAME'] = ('FIBERMAP', 'no dimension')
-			# Use the columns from fibermap with a few extras added.
-			columns = desispec.io.fibermap.fibermap_columns[:]
-			columns.extend([
-				('NIGHT','i4'),
-				('EXPID','i4'),
-				])
-			data = np.empty(shape = (0,),dtype = columns)
-			hdu4 = astropy.io.fits.BinTableHDU(data = data,header = hdr)
-			# Add comments for fibermap columns.
-			num_fibermap_columns = len(desispec.io.fibermap.fibermap_comments)
-			for i in range(1,1+num_fibermap_columns):
-				key = 'TTYPE%d' % i
-				name = hdu4.header[key]
-				comment = desispec.io.fibermap.fibermap_comments[name]
-				hdu4.header[key] = (name,comment)
-			# Add comments for our additional columns.
-			hdu4.header['TTYPE%d' % (1+num_fibermap_columns)] = ('NIGHT','Night of exposure YYYYMMDD')
-			hdu4.header['TTYPE%d' % (2+num_fibermap_columns)] = ('EXPID','Exposure ID')
-			self.hdu_list = astropy.io.fits.HDUList([hdu0,hdu1,hdu2,hdu3,hdu4])
-		else:
-			self.hdu_list = astropy.io.fits.open(path,mode = self.mode)
-			if len(self.hdu_list) != 5:
-				raise RuntimeError('Unexpected number of HDUs (%d) in %s' % (
-					len(self.hdu_list),self.path))
+    Raises:
+        RuntimeError: Invalid mode requested.
+        IOError: Unable to open existing file in 'readonly' mode.
+        OSError: Unable to create a new parent directory in 'update' mode.
+    """
+    def __init__(self,path,mode = 'readonly',header = None):
+        if mode not in ('readonly','update'):
+            raise RuntimeError('Invalid mode %r' % mode)
+        self.path = path
+        self.mode = mode
+        # Create a new file if necessary.
+        if self.mode == 'update' and not os.path.exists(self.path):
+            # Create the parent directory, if necessary.
+            head,tail = os.path.split(self.path)
+            if not os.path.exists(head):
+                os.makedirs(head)
+            # Create empty HDUs. It would be good to refactor io.frame to avoid any duplication here.
+            hdr = desispec.io.util.fitsheader(header)
+            hdr['EXTNAME'] = ('FLUX', 'no dimension')
+            hdu0 = astropy.io.fits.PrimaryHDU(header = hdr)
+            hdr['EXTNAME'] = ('IVAR', 'no dimension')
+            hdu1 = astropy.io.fits.ImageHDU(header = hdr)
+            hdr['EXTNAME'] = ('WAVELENGTH', '[Angstroms]')
+            hdu2 = astropy.io.fits.ImageHDU(header = hdr)
+            hdr['EXTNAME'] = ('RESOLUTION', 'no dimension')
+            hdu3 = astropy.io.fits.ImageHDU(header = hdr)
+            hdr['EXTNAME'] = ('FIBERMAP', 'no dimension')
+            # Use the columns from fibermap with a few extras added.
+            columns = desispec.io.fibermap.fibermap_columns[:]
+            columns.extend([
+                ('NIGHT','i4'),
+                ('EXPID','i4'),
+                ])
+            data = np.empty(shape = (0,),dtype = columns)
+            hdu4 = astropy.io.fits.BinTableHDU(data = data,header = hdr)
+            # Add comments for fibermap columns.
+            num_fibermap_columns = len(desispec.io.fibermap.fibermap_comments)
+            for i in range(1,1+num_fibermap_columns):
+                key = 'TTYPE%d' % i
+                name = hdu4.header[key]
+                comment = desispec.io.fibermap.fibermap_comments[name]
+                hdu4.header[key] = (name,comment)
+            # Add comments for our additional columns.
+            hdu4.header['TTYPE%d' % (1+num_fibermap_columns)] = ('NIGHT','Night of exposure YYYYMMDD')
+            hdu4.header['TTYPE%d' % (2+num_fibermap_columns)] = ('EXPID','Exposure ID')
+            self.hdu_list = astropy.io.fits.HDUList([hdu0,hdu1,hdu2,hdu3,hdu4])
+        else:
+            self.hdu_list = astropy.io.fits.open(path,mode = self.mode)
+            if len(self.hdu_list) != 5:
+                raise RuntimeError('Unexpected number of HDUs (%d) in %s' % (
+                    len(self.hdu_list),self.path))
 
-	def add_objects(self,flux,ivar,wave,resolution,object_data,night,expid):
-		"""
-		Add a list of objects to this brick file from the same night and exposure.
+    def add_objects(self,flux,ivar,wave,resolution,object_data,night,expid):
+        """
+        Add a list of objects to this brick file from the same night and exposure.
 
-		Args:
-			flux(numpy.ndarray): Array of (nobj,nwave) flux values for nobj objects tabulated
-				at nwave wavelengths.
-			ivar(numpy.ndarray): Array of (nobj,nwave) inverse-variance values.
-			wave(numpy.ndarray): Array of (nwave,) wavelength values in Angstroms. All objects
-				are assumed to use the same wavelength grid.
-			resolution(numpy.ndarray): Array of (nobj,nres,nwave) resolution matrix elements.
-			object_data(numpy.ndarray): Record array of fibermap rows for the objects to add.
-			night(str): Date string for the night these objects were observed in the format YYYYMMDD.
-			expid(int): Exposure number for these objects.
+        Args:
+            flux(numpy.ndarray): Array of (nobj,nwave) flux values for nobj objects tabulated
+                at nwave wavelengths.
+            ivar(numpy.ndarray): Array of (nobj,nwave) inverse-variance values.
+            wave(numpy.ndarray): Array of (nwave,) wavelength values in Angstroms. All objects
+                are assumed to use the same wavelength grid.
+            resolution(numpy.ndarray): Array of (nobj,nres,nwave) resolution matrix elements.
+            object_data(numpy.ndarray): Record array of fibermap rows for the objects to add.
+            night(str): Date string for the night these objects were observed in the format YYYYMMDD.
+            expid(int): Exposure number for these objects.
 
-		Raises:
-			RuntimeError: Cn only add objects in update mode.
-		"""
-		if self.mode != 'update':
-			raise RuntimeError('Can only add objects in update mode.')
-		# Augment object_data with constant NIGHT and EXPID columns.
-		augmented_data = np.empty(shape = object_data.shape,dtype = self.hdu_list[4].data.dtype)
-		for column_def in desispec.io.fibermap.fibermap_columns:
-			name = column_def[0]
-			if name == 'FILTER' and augmented_data[name].shape != object_data[name].shape:
-				for i,filters in enumerate(object_data[name]):
-					augmented_data[name][i] = ','.join(filters)
-			else:
-				augmented_data[name] = object_data[name]
-		augmented_data['NIGHT'] = int(night)
-		augmented_data['EXPID'] = expid
-		# Concatenate the new per-object image HDU data or use it to initialize the HDU.
-		# HDU2 contains the wavelength grid shared by all objects so we only add it once.
-		if self.hdu_list[0].data is not None:
-			self.hdu_list[0].data = np.concatenate((self.hdu_list[0].data,flux,))
-			self.hdu_list[1].data = np.concatenate((self.hdu_list[1].data,ivar,))
-			assert np.array_equal(self.hdu_list[2].data,wave),'Wavelength arrays do not match.'
-			self.hdu_list[3].data = np.concatenate((self.hdu_list[3].data,resolution,))
-		else:
-			self.hdu_list[0].data = flux
-			self.hdu_list[1].data = ivar
-			self.hdu_list[2].data = wave
-			self.hdu_list[3].data = resolution
-		# Always concatenate our table since a new file will be created with a zero-length table.
-		self.hdu_list[4].data = np.concatenate((self.hdu_list[4].data,augmented_data,))
+        Raises:
+            RuntimeError: Cn only add objects in update mode.
+        """
+        if self.mode != 'update':
+            raise RuntimeError('Can only add objects in update mode.')
+        # Augment object_data with constant NIGHT and EXPID columns.
+        augmented_data = np.empty(shape = object_data.shape,dtype = self.hdu_list[4].data.dtype)
+        for column_def in desispec.io.fibermap.fibermap_columns:
+            name = column_def[0]
+            if name == 'FILTER' and augmented_data[name].shape != object_data[name].shape:
+                for i,filters in enumerate(object_data[name]):
+                    augmented_data[name][i] = ','.join(filters)
+            else:
+                augmented_data[name] = object_data[name]
+        augmented_data['NIGHT'] = int(night)
+        augmented_data['EXPID'] = expid
+        # Concatenate the new per-object image HDU data or use it to initialize the HDU.
+        # HDU2 contains the wavelength grid shared by all objects so we only add it once.
+        if self.hdu_list[0].data is not None:
+            self.hdu_list[0].data = np.concatenate((self.hdu_list[0].data,flux,))
+            self.hdu_list[1].data = np.concatenate((self.hdu_list[1].data,ivar,))
+            assert np.array_equal(self.hdu_list[2].data,wave),'Wavelength arrays do not match.'
+            self.hdu_list[3].data = np.concatenate((self.hdu_list[3].data,resolution,))
+        else:
+            self.hdu_list[0].data = flux
+            self.hdu_list[1].data = ivar
+            self.hdu_list[2].data = wave
+            self.hdu_list[3].data = resolution
+        # Always concatenate our table since a new file will be created with a zero-length table.
+        self.hdu_list[4].data = np.concatenate((self.hdu_list[4].data,augmented_data,))
 
-	def get_num_objects(self):
-		"""
-		Get the number of objects contained in this brick file.
+    def get_num_objects(self):
+        """
+        Get the number of objects contained in this brick file.
 
-		Returns:
-			int: Number of objects contained in this brick file.
-		"""
-		return len(self.hdu_list[0].data)
+        Returns:
+            int: Number of objects contained in this brick file.
+        """
+        return len(self.hdu_list[0].data)
 
-	def close(self):
-		"""
-		Write any updates and close the brick file.
-		"""
-		if self.mode == 'update':
-			self.hdu_list.writeto(self.path,clobber = True)
-		self.hdu_list.close()
+    def close(self):
+        """
+        Write any updates and close the brick file.
+        """
+        if self.mode == 'update':
+            self.hdu_list.writeto(self.path,clobber = True)
+        self.hdu_list.close()
