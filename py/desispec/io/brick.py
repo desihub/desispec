ee23acf9c6d2928d08d708d050250fc79f50de10
David Kirkby <dkirkby@uci.edu>
2015-03-18 15:38:07 -0700

    Refactor Brick to use BrickBase for stuff that will be reused for the CoAdd class

--- py/desispec/io/brick.py
+++ py/desispec/io/brick.py
@@ -1,8 +1,8 @@
 """
-I/O routines for working with brick files.
+I/O routines for working with per-brick files.
 
-See doc/DESI_SPECTRO_REDUX/PRODNAME/bricks/BRICKID/brick-BRICKID.rst in desiDataModel
-for a description of the brick file data model.
+See doc/DESI_SPECTRO_REDUX/PRODNAME/bricks/BRICKID/*-BRICKID.rst in desiDataModel
+for a description of the relevant data models.
 """
 
 import os
@@ -13,7 +13,7 @@ import astropy.io.fits
 
 import desispec.io.util
 
-class Brick(object):
+class BrickBase(object):
     """
     Represents objects in a single brick and possibly also a single band b,r,z.
 
@@ -56,12 +56,79 @@ class Brick(object):
             hdr['EXTNAME'] = ('RESOLUTION', 'no dimension')
             hdu3 = astropy.io.fits.ImageHDU(header = hdr)
             hdr['EXTNAME'] = ('FIBERMAP', 'no dimension')
-            # Use the columns from fibermap with a few extras added.
+            self.hdu_list = astropy.io.fits.HDUList([hdu0,hdu1,hdu2,hdu3,])
+        else:
+            self.hdu_list = astropy.io.fits.open(path,mode = self.mode)
+            if len(self.hdu_list) != 5:
+                raise RuntimeError('Unexpected number of HDUs (%d) in %s' % (
+                    len(self.hdu_list),self.path))
+
+    def create_table(self):
+        pass
+
+    def add_objects(self,flux,ivar,wave,resolution):
+        """
+        Add a list of objects to this brick file from the same night and exposure.
+
+        Args:
+            flux(numpy.ndarray): Array of (nobj,nwave) flux values for nobj objects tabulated
+                at nwave wavelengths.
+            ivar(numpy.ndarray): Array of (nobj,nwave) inverse-variance values.
+            wave(numpy.ndarray): Array of (nwave,) wavelength values in Angstroms. All objects
+                are assumed to use the same wavelength grid.
+            resolution(numpy.ndarray): Array of (nobj,nres,nwave) resolution matrix elements.
+
+        Raises:
+            RuntimeError: Can only add objects in update mode.
+        """
+        if self.mode != 'update':
+            raise RuntimeError('Can only add objects in update mode.')
+        # Concatenate the new per-object image HDU data or use it to initialize the HDU.
+        # HDU2 contains the wavelength grid shared by all objects so we only add it once.
+        if self.hdu_list[0].data is not None:
+            self.hdu_list[0].data = np.concatenate((self.hdu_list[0].data,flux,))
+            self.hdu_list[1].data = np.concatenate((self.hdu_list[1].data,ivar,))
+            assert np.array_equal(self.hdu_list[2].data,wave),'Wavelength arrays do not match.'
+            self.hdu_list[3].data = np.concatenate((self.hdu_list[3].data,resolution,))
+        else:
+            self.hdu_list[0].data = flux
+            self.hdu_list[1].data = ivar
+            self.hdu_list[2].data = wave
+            self.hdu_list[3].data = resolution
+
+    def get_num_objects(self):
+        """
+        Get the number of objects contained in this brick file.
+
+        Returns:
+            int: Number of objects contained in this brick file.
+        """
+        return len(self.hdu_list[0].data)
+
+    def close(self):
+        """
+        Write any updates and close the brick file.
+        """
+        if self.mode == 'update':
+            self.hdu_list.writeto(self.path,clobber = True)
+        self.hdu_list.close()
+
+class Brick(BrickBase):
+    """
+    Represents the combined cframe exposures in a single brick and band.
+
+    See :class:`BrickBase` for constructor info.
+    """
+    def __init__(self,path,mode = 'readonly',header = None):
+        BrickBase.__init__(self,path,mode,header)
+        if self.mode == 'update' and not os.path.exists(self.path):
+            # Create an HDU4 using the columns from fibermap with a few extras added.
             columns = desispec.io.fibermap.fibermap_columns[:]
             columns.extend([
                 ('NIGHT','i4'),
                 ('EXPID','i4'),
                 ])
+            hdr = desispec.io.util.fitsheader(header)
             data = np.empty(shape = (0,),dtype = columns)
             hdu4 = astropy.io.fits.BinTableHDU(data = data,header = hdr)
             # Add comments for fibermap columns.
@@ -74,12 +141,7 @@ class Brick(object):
             # Add comments for our additional columns.
             hdu4.header['TTYPE%d' % (1+num_fibermap_columns)] = ('NIGHT','Night of exposure YYYYMMDD')
             hdu4.header['TTYPE%d' % (2+num_fibermap_columns)] = ('EXPID','Exposure ID')
-            self.hdu_list = astropy.io.fits.HDUList([hdu0,hdu1,hdu2,hdu3,hdu4])
-        else:
-            self.hdu_list = astropy.io.fits.open(path,mode = self.mode)
-            if len(self.hdu_list) != 5:
-                raise RuntimeError('Unexpected number of HDUs (%d) in %s' % (
-                    len(self.hdu_list),self.path))
+            self.hdu_list.append(hdu4)
 
     def add_objects(self,flux,ivar,wave,resolution,object_data,night,expid):
         """
@@ -99,8 +161,7 @@ class Brick(object):
         Raises:
             RuntimeError: Can only add objects in update mode.
         """
-        if self.mode != 'update':
-            raise RuntimeError('Can only add objects in update mode.')
+        BrickBase.add_objects(self,flux,ivar,wave,resolution)
         # Augment object_data with constant NIGHT and EXPID columns.
         augmented_data = np.empty(shape = object_data.shape,dtype = self.hdu_list[4].data.dtype)
         for column_def in desispec.io.fibermap.fibermap_columns:
@@ -112,34 +173,5 @@ class Brick(object):
                 augmented_data[name] = object_data[name]
         augmented_data['NIGHT'] = int(night)
         augmented_data['EXPID'] = expid
-        # Concatenate the new per-object image HDU data or use it to initialize the HDU.
-        # HDU2 contains the wavelength grid shared by all objects so we only add it once.
-        if self.hdu_list[0].data is not None:
-            self.hdu_list[0].data = np.concatenate((self.hdu_list[0].data,flux,))
-            self.hdu_list[1].data = np.concatenate((self.hdu_list[1].data,ivar,))
-            assert np.array_equal(self.hdu_list[2].data,wave),'Wavelength arrays do not match.'
-            self.hdu_list[3].data = np.concatenate((self.hdu_list[3].data,resolution,))
-        else:
-            self.hdu_list[0].data = flux
-            self.hdu_list[1].data = ivar
-            self.hdu_list[2].data = wave
-            self.hdu_list[3].data = resolution
         # Always concatenate our table since a new file will be created with a zero-length table.
         self.hdu_list[4].data = np.concatenate((self.hdu_list[4].data,augmented_data,))
-
-    def get_num_objects(self):
-        """
-        Get the number of objects contained in this brick file.
-
-        Returns:
-            int: Number of objects contained in this brick file.
-        """
-        return len(self.hdu_list[0].data)
-
-    def close(self):
-        """
-        Write any updates and close the brick file.
-        """
-        if self.mode == 'update':
-            self.hdu_list.writeto(self.path,clobber = True)
-        self.hdu_list.close()
