4481c125e791370435675462bdf2ff7d51feffce
Stephen Bailey <stephen.code@gmail.com>
2015-04-14 10:39:26 +0900

    added start of copyprod script

---
+++ bin/copyprod.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+"""
+Copy a DESI production from one directory to another
+
+Currently this only copies the directory tree and the extracted frame files.
+This script will be expanded to support copying through any level of
+processing, including the metadata of how we got to that processing step.
+
+Stephen Bailey
+April 2015
+"""
+
+import os
+import shutil
+import optparse
+
+parser = optparse.OptionParser(usage = "%prog [options] indir outdir")
+# parser.add_option("-i", "--input", type="string",  help="input data")
+# parser.add_option("-x", "--xxx",   help="some flag", action="store_true")
+opts, args = parser.parse_args()
+
+inroot, outroot = args
+
+if not os.path.exists(outroot):
+    os.makedirs(outroot)
+
+#- Copy exposures
+for indir, subdirs, filenames in os.walk(inroot+'/exposures'):
+    outdir = indir.replace(inroot, outroot)
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+
+    for name in filenames:
+        if name.startswith('frame-'):
+            shutil.copy2(indir+'/'+name, outdir+'/'+name)
+            
+
