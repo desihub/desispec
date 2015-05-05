6fe7949c0bf172b3b52a3142a186fabff70bc57d
Benjamin Alan Weaver <benjamin.weaver@nyu.edu>
2015-05-05 17:46:56 -0400

    clean up docstrings and add api hooks

--- doc/api.rst
+++ doc/api.rst
@@ -10,3 +10,33 @@ The desispec API
 
 .. automodule:: desispec.coaddition
     :members:
+
+.. automodule:: desispec.fiberflat
+    :members:
+
+.. automodule:: desispec.fluxcalibration
+    :members:
+
+.. automodule:: desispec.interpolation
+    :members:
+
+.. automodule:: desispec.linalg
+    :members:
+
+.. automodule:: desispec.log
+    :members:
+
+.. automodule:: desispec.maskbits
+    :members:
+
+.. automodule:: desispec.resolution
+    :members:
+
+.. automodule:: desispec.sky
+    :members:
+
+.. automodule:: desispec.io
+    :members:
+
+.. automodule:: desispec.io.brick
+    :members:
--- py/desispec/maskbits.py
+++ py/desispec/maskbits.py
@@ -2,24 +2,27 @@
 # BITS INTO ONE PLACE.
 
 """
+desispec.maskbits
+=================
+
 Mask bits for the spectro pipeline.
 
 Stephen Bailey, LBNL, January 2015
 
-Example:
+Example::
 
-from desispec.maskbits import ccdmask
+    from desispec.maskbits import ccdmask
 
-ccdmask.COSMIC | specmask.SATURATED
-ccdmask.mask('COSMIC')     #- 2**0, same as ccdmask.COSMIC
-ccdmask.mask(0)            #- 2**0, same as ccdmask.COSMIC
-ccdmask.COSMIC             #- 2**0, same as ccdmask.mask('COSMIC')
-ccdmask.bitnum('COSMIC')   #- 0
-ccdmask.bitname(0)         #- 'COSMIC'
-ccdmask.names()            #- ['COSMIC', 'HOT', 'DEAD', 'SATURATED', ...]
-ccdmask.names(3)           #- ['COSMIC', 'HOT']
-ccdmask.comment(0)         #- "Cosmic ray" 
-ccdmask.comment('BADPIX')  #- "Cosmic ray"
+    ccdmask.COSMIC | specmask.SATURATED
+    ccdmask.mask('COSMIC')     #- 2**0, same as ccdmask.COSMIC
+    ccdmask.mask(0)            #- 2**0, same as ccdmask.COSMIC
+    ccdmask.COSMIC             #- 2**0, same as ccdmask.mask('COSMIC')
+    ccdmask.bitnum('COSMIC')   #- 0
+    ccdmask.bitname(0)         #- 'COSMIC'
+    ccdmask.names()            #- ['COSMIC', 'HOT', 'DEAD', 'SATURATED', ...]
+    ccdmask.names(3)           #- ['COSMIC', 'HOT']
+    ccdmask.comment(0)         #- "Cosmic ray"
+    ccdmask.comment('BADPIX')  #- "Cosmic ray"
 """
 
 #- Move these definitions into a separate yaml file
@@ -31,7 +34,7 @@ ccdmask:
     - [HOT,       1, "Hot pixel"]
     - [DEAD,      2, "Dead pixel"]
     - [SATURATED, 3, "Saturated pixel from object"]
-    
+
 #- Mask bits that apply to an entire fiber
 fibermask:
     - [BADFIBER,     0, "Broken or otherwise unusable fiber"]
@@ -50,20 +53,22 @@ spmask:
     - [BADFIBERFLAT, 4, "Bad fiber flat solution"]
     - [BRIGHTSKY,    5, "Bright sky level (details TBD)"]
     - [BADSKY,       6, "Bad sky model"]
-    
+
 #- zmask: reasons why redshift fitting failed
 """)
 
 #- Class to provide mask bit utility functions
 class BitMask(object):
+    """BitMask object.
+    """
     def __init__(self, name, bitdefs):
         """
-        name : name of this mask, must be key in bitdefs
-        bitdefs : dictionary of different mask bit definitions
-            each value is a list of [bitname, bitnum, comment]
-            
+        Args:
+            name : name of this mask, must be key in bitdefs
+            bitdefs : dictionary of different mask bit definitions each value is a list of [bitname, bitnum, comment]
+
         Users are not expected to create BitMask objects directly.
-        
+
         See maskbits.ccdmask, maskbits.spmask, maskbits.fibermask, ...
         """
         self._name = name
@@ -77,29 +82,28 @@ class BitMask(object):
             self._bitname[bitnum] = bitname
             self._comment[bitname] = comment
             self._comment[bitnum] = comment
-        
+
     def bitnum(self, bitname):
         """Return bit number (int) for bitname (string)"""
         return self._bitnum[bitname]
-        
+
     def bitname(self, bitnum):
         """Return bit name (string) for this bitnum (integer)"""
         return self._bitname[bitnum]
-        
+
     def comment(self, bitname_or_num):
         """Return comment for this bit name or bit number"""
         return self._comment[bitname_or_num]
-        
+
     def mask(self, name_or_num):
         """Return mask value, i.e. 2**bitnum for this name or number"""
         if isinstance(name_or_num, int):
             return 2**name_or_num
         else:
             return 2**self._bitnum[name_or_num]
-            
+
     def names(self, mask=None):
-        """
-        Return list of names of masked bits.
+        """Return list of names of masked bits.
         If mask=None, return names of all known bits.
         """
         names = list()
@@ -115,29 +119,27 @@ class BitMask(object):
                     else:
                         names.append('UNKNOWN'+str(bitnum))
                 bitnum += 1
-                
+
         return names
-            
+
     #- Allow access via mask.BITNAME
     def __getattr__(self, name):
         if name in self._bitnum:
             return 2**self._bitnum[name]
         else:
             raise AttributeError('Unknown mask bit name '+name)
-            
+
     #- What to print
     def __repr__(self):
         result = list()
         result.append( self._name+':' )
         for i in sorted(self._bitname.keys()):
             result.append('    - [{:16s} {:2d}, "{}"]'.format(self._bitname[i]+',', i, self._comment[i]))
-            
+
         return "\n".join(result)
 
 #-------------------------------------------------------------------------
-#- The actual masks            
+#- The actual masks
 spmask = BitMask('spmask', _bitdefs)
 ccdmask = BitMask('ccdmask', _bitdefs)
 fibermask = BitMask('fibermask', _bitdefs)
-
-
