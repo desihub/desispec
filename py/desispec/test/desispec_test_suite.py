6fca7192927b80c1f95fe1fcbb175a594605ec86
Stephen Bailey <stephen.code@gmail.com>
2015-05-07 15:02:53 -0700

    refactoring tests is failing

--- py/desispec/test/desispec_test_suite.py
+++ py/desispec/test/desispec_test_suite.py
@@ -10,6 +10,30 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import unittest
 from os.path import dirname
 #
+#- This is factored out separately from runtests() so that it can be used by
+#- python setup.py test
 def desispec_test_suite():
+    """Returns unittest.TestSuite of desispec tests"""
     desispec_dir = dirname(dirname(__file__))
     return unittest.defaultTestLoader.discover(desispec_dir)
+
+def runtests():
+    """
+    Run all tests in desispec.test.test_*
+    """
+    #- Load all TestCase classes from desispec/test/test_*.py
+    tests = desispec_test_suite()
+    
+    #- Run them
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+def orig_runtests():
+    """
+    Run all tests in desispec.test.test_*
+    """
+    import os.path
+    #- Load all TestCase classes from desispec/test/test_*.py
+    tests = unittest.TestLoader().discover(os.path.dirname(__file__))
+        
+    #- Run them
+    unittest.TextTestRunner(verbosity=2).run(tests)
