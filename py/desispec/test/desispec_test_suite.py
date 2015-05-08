f749f60532043ee5aabb01eab9d7ae58625bdc69
Benjamin Alan Weaver <benjamin.weaver@nyu.edu>
2015-05-08 17:40:51 -0400

    get the runtests() function working

--- py/desispec/test/desispec_test_suite.py
+++ py/desispec/test/desispec_test_suite.py
@@ -8,32 +8,21 @@ Used to initialize the unit test framework via ``python setup.py test``.
 from __future__ import absolute_import, division, print_function, unicode_literals
 #
 import unittest
-from os.path import dirname
 #
 #- This is factored out separately from runtests() so that it can be used by
 #- python setup.py test
 def desispec_test_suite():
     """Returns unittest.TestSuite of desispec tests"""
+    from os.path import dirname
     desispec_dir = dirname(dirname(__file__))
-    return unittest.defaultTestLoader.discover(desispec_dir)
+    # print(desispec_dir)
+    return unittest.defaultTestLoader.discover(desispec_dir,
+        top_level_dir=dirname(desispec_dir))
 
 def runtests():
-    """
-    Run all tests in desispec.test.test_*
+    """Run all tests in desispec.test.test_*.
     """
     #- Load all TestCase classes from desispec/test/test_*.py
     tests = desispec_test_suite()
-    
-    #- Run them
-    unittest.TextTestRunner(verbosity=2).run(tests)
-
-def orig_runtests():
-    """
-    Run all tests in desispec.test.test_*
-    """
-    import os.path
-    #- Load all TestCase classes from desispec/test/test_*.py
-    tests = unittest.TestLoader().discover(os.path.dirname(__file__))
-        
     #- Run them
     unittest.TextTestRunner(verbosity=2).run(tests)
