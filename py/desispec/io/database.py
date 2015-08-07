fa13bcd85295789c2073e203b252d7ff19c38e15
Benjamin Alan Weaver <benjamin.weaver@nyu.edu>
2015-08-07 16:19:37 -0400

    add load_night

--- py/desispec/io/database.py
+++ py/desispec/io/database.py
@@ -27,7 +27,7 @@ def load_brick(fitsfile,dbfile,fix_area=False):
     bricklist = [ brickdata[col].tolist() for col in brickdata.names ]
     if fix_area:
         area = np.degrees(
-            (brickdata['ra2'] - birckdata['ra1'])
+            (brickdata['ra2'] - brickdata['ra1'])
             * (np.sin(np.radians(brickdata['dec2'])) - np.sin(np.radians(brickdata['dec2']))))
         bricklist.append(area.tolist())
     conn = sqlite3.connect(dbfile)
@@ -36,6 +36,33 @@ def load_brick(fitsfile,dbfile,fix_area=False):
         (brickname, brickid, brickq, brickrow, brickcol, ra, dec, ra1, ra2, dec1, dec2, area)
         VALUES (?,?,?,?,?,?,?,?,?,?,?,?);"""
     c.executemany(insert,zip(*bricklist))
+    conn.commit()
+    conn.close()
+    return
+#
+#
+#
+def load_night(nights,dbfile):
+    """Load a night or multiple nights into the night table.
+
+    Args:
+        nights: integer, string or list of nights
+        dbfile: string containing the name of a SQLite database file.
+
+    Returns:
+        None
+    """
+    if isinstance(nights,str):
+        my_nights = [int(nights)]
+    elif isinstance(nights,int):
+        my_nights = [nights]
+    else:
+        my_nights = [int(n) for n in nights]
+    conn = sqlite3.connect(dbfile)
+    c = conn.cursor()
+    insert = """INSERT INTO night (night)
+        VALUES (?);"""
+    c.executemany(insert,zip(my_nights))
     conn.commit()
     conn.close()
     return
