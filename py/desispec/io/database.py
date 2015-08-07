ba04fc1b63435461603aaabe153b163a497cf3dc
Benjamin Alan Weaver <benjamin.weaver@nyu.edu>
2015-08-07 15:15:35 -0400

    deal with missing area column

--- py/desispec/io/database.py
+++ py/desispec/io/database.py
@@ -7,15 +7,17 @@ Code for interacting with the file metadatabase.
 from __future__ import absolute_import, division, print_function
 import sqlite3
 from astropy.io import fits
+import numpy as np
 #
 #
 #
-def load_brick(fitsfile,dbfile):
+def load_brick(fitsfile,dbfile,fix_area=False):
     """Load a bricks FITS file into the database.
 
     Args:
         fitsfile: string containing the name of a bricks file.
         dbfile: string containing the name of a SQLite database file.
+        fix_area: (optional) If ``True``, deal with missing area column.
 
     Returns:
         None
@@ -23,6 +25,11 @@ def load_brick(fitsfile,dbfile):
     with fits.open(fitsfile) as f:
         brickdata = f[1].data
     bricklist = [ brickdata[col].tolist() for col in brickdata.names ]
+    if fix_area:
+        area = np.degrees(
+            (brickdata['ra2'] - birckdata['ra1'])
+            * (np.sin(np.radians(brickdata['dec2'])) - np.sin(np.radians(brickdata['dec2']))))
+        bricklist.append(area.tolist())
     conn = sqlite3.connect(dbfile)
     c = conn.cursor()
     insert = """INSERT INTO brick
