72934e60442bca8ad83bfe2f60cfbe5a731ddd15
Julien Guy <guy@lpnhe.in2p3.fr>
2015-09-16 16:49:46 +0200

    a very stupid test

---
+++ py/desispec/test/test_cosmics.py
@@ -0,0 +1,32 @@
+"""
+test desispec.cosmics
+"""
+
+import unittest
+import numpy as np
+from desispec.image import Image
+from desispec.cosmics import reject_cosmic_rays_ala_sdss
+from desispec.log import get_logger
+
+#- Create a DESI logger at level WARNING to quiet down the fiberflat calc
+import logging
+log = get_logger(logging.WARNING)
+
+
+class TestCosmics(unittest.TestCase):
+
+    def test_rejection_ala_sdss(self):
+        """
+        Very basic test of reject_cosmic_rays_ala_sdss
+        """
+        pix=np.zeros((53,50))
+        ivar=np.ones(pix.shape)
+        pix[12,12]=100
+        image = Image(pix,ivar)        
+        rejected=reject_cosmic_rays_ala_sdss(image,dilate=False)
+        diff=np.sum(np.abs((pix>0).astype(int) - rejected.astype(int)))
+        self.assertTrue(diff==0)
+        
+#- This runs all test* functions in any TestCase class in this file
+if __name__ == '__main__':
+    unittest.main()           
