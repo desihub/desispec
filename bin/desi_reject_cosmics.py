fb873bbbc57c14af48099fa7731d122f8394f25c
Julien Guy <guy@lpnhe.in2p3.fr>
2015-09-17 13:55:28 +0200

    use a generic routine from cosmics.py

--- bin/desi_reject_cosmics.py
+++ bin/desi_reject_cosmics.py
@@ -10,7 +10,7 @@ This script finds cosmics in a pre-processed image and write the result in the m
 """
 
 from desispec.io import image
-from desispec.cosmics import reject_cosmic_rays_ala_sdss
+from desispec.cosmics import reject_cosmic_rays
 from desispec.log import get_logger
 import argparse
 import numpy as np
@@ -34,23 +34,15 @@ def main() :
     
 
     log = get_logger()
-    
     log.info("starting finding cosmics in %s"%args.infile)
     
     img=image.read_image(args.infile)
     
-    log.warning("temporary hack: ignore the mask to find cosmics !!")
-    image_mask=img.mask.copy()
-    img._mask = np.zeros(img.pix.shape)
+    reject_cosmic_rays(img)
     
-    rejected=reject_cosmic_rays_ala_sdss(img,psf_sigma_pix=1.)
-    # copy this in the original image mask
-    img._mask= image_mask + rejected*(image_mask==0)
-
     log.info("writing data and new mask in %s"%outfile)
     image.write_image(outfile, img, meta=img.meta)
-    
-    
+        
     log.info("done")
     
 if __name__ == '__main__':
