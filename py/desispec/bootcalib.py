5d75b5c3e52b59d9adc59388a71f3983a9a755c8
Stephen Bailey <stephen.code@gmail.com>
2016-05-03 13:34:30 -0700

    move wrap_bootcalib into desispec.bootcalib.bootcalib()

--- py/desispec/bootcalib.py
+++ py/desispec/bootcalib.py
@@ -43,6 +43,76 @@ except KeyError:
 glbl_figsz = (16,9)
 
 ########################################################
+# High level wrapper
+# TODO: This was ported from the original bin/desi_bootcalib so that it could
+# be called independently by quicklook, but it needs to be coordinated with
+# desispec.scripts.bootcalib.main()
+########################################################
+
+def bootcalib(deg,flatimage,arcimage):
+    """
+    deg: Legendre polynomial degree to use to fit
+    flatimage: desispec.image.Image object of flatfield
+    arcimage: desispec.image.Image object of arc
+
+    #- Mostly inherited from desispec/bin/desi_bootcalib directly as needed
+
+    returns xfit
+            fdicts
+            gauss
+            all_wave_soln
+    TODO: document what those return objects are
+    """
+
+    camera=flatimage.camera
+    flat=flatimage.pix
+    ny=flat.shape[0]
+
+    xpk,ypos,cut=find_fiber_peaks(flat)
+    xset,xerr=trace_crude_init(flat,xpk,ypos)
+    xfit,fdicts=fit_traces(xset,xerr)
+    gauss=fiber_gauss(flat,xfit,xerr)
+
+    #- Also need wavelength solution not just trace
+
+    arc=arcimage.pix
+    all_spec=extract_sngfibers_gaussianpsf(arc,xfit,gauss)
+    llist=load_arcline_list(camera)
+    dlamb,wmark,gd_lines,line_guess=load_gdarc_lines(camera)
+
+    #- Solve for wavelengths
+    all_wv_soln=[]
+    all_dlamb=[]
+    for ii in range(all_spec.shape[1]):
+        spec=all_spec[:,ii]
+        pixpk=find_arc_lines(spec)
+        id_dict=id_arc_lines(pixpk,gd_lines,dlamb,wmark,line_guess=line_guess)
+        id_dict['fiber']=ii
+        #- Find the other good ones
+        if camera == 'z':
+            inpoly = 3  # The solution in the z-camera has greater curvature
+        else:
+            inpoly = 2
+        add_gdarc_lines(id_dict, pixpk, gd_lines, inpoly=inpoly)
+        #- Now the rest
+        id_remainder(id_dict, pixpk, llist)
+        #- Final fit wave vs. pix too
+        final_fit, mask = dufits.iter_fit(np.array(id_dict['id_wave']), np.array(id_dict['id_pix']), 'polynomial', 3, xmin=0., xmax=1.)
+        rms = np.sqrt(np.mean((dufits.func_val(np.array(id_dict['id_wave'])[mask==0],final_fit)-np.array(id_dict['id_pix'])[mask==0])**2))
+        final_fit_pix,mask2 = dufits.iter_fit(np.array(id_dict['id_pix']), np.array(id_dict['id_wave']),'legendre',deg, niter=5)
+
+        id_dict['final_fit'] = final_fit
+        id_dict['rms'] = rms
+        id_dict['final_fit_pix'] = final_fit_pix
+        id_dict['wave_min'] = dufits.func_val(0,final_fit_pix)
+        id_dict['wave_max'] = dufits.func_val(ny-1,final_fit_pix)
+        id_dict['mask'] = mask
+        all_wv_soln.append(id_dict)
+
+    return xfit, fdicts, gauss,all_wv_soln
+
+
+########################################################
 # Arc/Wavelength Routines (Linelists come next)
 ########################################################
 
