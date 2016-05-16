fdd1048f8d9fd883e7beaccebcbaa3ea331c1ccb
Stephen Bailey <StephenBailey@lbl.gov>
2016-05-15 18:52:46 -0700

    agg backend (not Agg)

--- py/desispec/qa/qa_plots.py
+++ py/desispec/qa/qa_plots.py
@@ -11,6 +11,7 @@ import pdb
 from desispec import fluxcalibration as dsflux
 
 import matplotlib
+matplotlib.use('agg')
 from matplotlib import pyplot as plt
 import matplotlib.gridspec as gridspec
 
