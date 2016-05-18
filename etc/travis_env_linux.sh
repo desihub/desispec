7dc5aa366a51c3b08b84449911102d3d080cf24d
Theodore Kisner <mail@theodorekisner.com>
2016-05-17 22:56:06 -0700

    Update travis environment to include harp, specex, and specter.

--- etc/travis_env_linux.sh
+++ etc/travis_env_linux.sh
@@ -4,7 +4,7 @@
 wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
 chmod +x miniconda.sh
 ./miniconda.sh -b -p $HOME/miniconda
-export PATH=/home/travis/miniconda/bin:$PATH
+export PATH=$HOME/miniconda/bin:$PATH
 conda update --yes conda
 
 # Installation of non-Python dependencies for documentation is now
