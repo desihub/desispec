1a26441da56ce1ce32132d6c3162be8e91f3cba3
Theodore Kisner <tskisner@users.noreply.github.com>
2016-07-06 14:30:15 -0700

    MPI zfind (#238)
    * Initial implementation of lightweight MPI wrapper around redmonster.  Still to be tested.
    
    * MPI parallel redmonster wrapper now runs and outputs agree with multiprocessing case.  Still need to reduce / organize per-process logging in order to make the logs readable/useful.
    
    * Small bugfix for zfind MPI wrapper.  When running in MPI mode, force redmonster logging level to WARNING, otherwise logs are completely unreadable.  Use one node per brick for zfind.  Even for ~150 targets/brick this runs in ~15 minutes.
    
    * When generating slurm and shell scripts for the pipeline, also generate a master script which submits the individual jobs to the queue and sets up job dependencies.
    
    * Implement simple load balancing of bricks during redshift fitting.

--- py/desispec/scripts/zfind.py
+++ py/desispec/scripts/zfind.py
@@ -11,11 +11,11 @@ import traceback
 
 from desispec import io
 from desispec.interpolation import resample_flux
-from desispec.log import get_logger
+from desispec.log import get_logger, WARNING
 from desispec.zfind.redmonster import RedMonsterZfind
 from desispec.zfind import ZfindBase
 from desispec.io.qa import load_qa_brick, write_qa_brick
-from desispec.util import default_nproc
+from desispec.util import default_nproc, dist_uniform
 
 import argparse
 
@@ -62,18 +62,7 @@ def parse(options=None):
     return args
 
 
-#- function for multiprocessing
-def _func(arg) :
-    try:
-    	ret = RedMonsterZfind(**arg)
-    except Exception,e:
-	print str(e)
-	traceback.print_tb(sys.exc_info()[2])
-	raise
-    return ret
-
-
-def main(args) :
+def main(args, comm=None) :
 
     log = get_logger()
 
@@ -84,19 +73,29 @@ def main(args) :
         log.warning("Need nproc>=1, changing this %d -> 1"%args.nproc)
         args.nproc=1
     
+    if comm is not None:
+        if args.nproc != 1:
+            if comm.rank == 0:
+                log.warning("Using MPI, forcing multiprocessing nproc -> 1")
+            args.nproc = 1
 
     if args.objtype is not None:
         args.objtype = args.objtype.split(',')
 
     #- Read brick files for each channel
-    log.info("Reading bricks")
+    if (comm is None) or (comm.rank == 0):
+        log.info("Reading bricks")
     brick = dict()
     if args.brick is not None:
         if len(args.brickfiles) != 0:
             raise RuntimeError('Give -b/--brick or input brickfiles but not both')
         for channel in ('b', 'r', 'z'):
-            filename = io.findfile('brick', band=channel, brickname=args.brick,
-                                   specprod_dir=args.specprod_dir)
+            filename = None
+            if (comm is None) or (comm.rank == 0):
+                filename = io.findfile('brick', band=channel, brickname=args.brick,
+                                        specprod_dir=args.specprod_dir)
+            if comm is not None:
+                filename = comm.bcast(filename, root=0)
             brick[channel] = io.Brick(filename)
     else:
         for filename in args.brickfiles:
@@ -104,12 +103,14 @@ def main(args) :
             if bx.channel not in brick:
                 brick[bx.channel] = bx
             else:
-                log.error('Channel {} in multiple input files'.format(bx.channel))
+                if (comm is None) or (comm.rank == 0):
+                    log.error('Channel {} in multiple input files'.format(bx.channel))
                 sys.exit(2)
 
     filters=brick.keys()
     for fil in filters:
-        log.info("Filter found: "+fil)
+        if (comm is None) or (comm.rank == 0):
+            log.info("Filter found: "+fil)
 
     #- Assume all channels have the same number of targets
     #- TODO: generalize this to allow missing channels
@@ -122,7 +123,8 @@ def main(args) :
     #- Coadd individual exposures and combine channels
     #- Full coadd code is a bit slow, so try something quick and dirty for
     #- now to get something going for redshifting
-    log.info("Combining individual channels and exposures")
+    if (comm is None) or (comm.rank == 0):
+        log.info("Combining individual channels and exposures")
     wave=[]
     for fil in filters:
         wave=np.concatenate([wave,brick[fil].get_wavelength_grid()])
@@ -137,8 +139,10 @@ def main(args) :
     good_targetids=[]
     targetids = brick['b'].get_target_ids()
 
-    if not args.print_info is None:
-	    fpinfo=open(args.print_info,"w")
+    fpinfo = None
+    if args.print_info is not None:
+        if (comm is None) or (comm.rank == 0):
+            fpinfo = open(args.print_info,"w")
 
     for i, targetid in enumerate(targetids):
         #- wave, flux, and ivar for this target; concatenate
@@ -146,113 +150,215 @@ def main(args) :
         xflux = list()
         xivar = list()
 
-	good=True
+        good=True
         for channel in filters:
             exp_flux, exp_ivar, resolution, info = brick[channel].get_target(targetid)
             weights = np.sum(exp_ivar, axis=0)
-	    ii, = np.where(weights > 0)
-	    if len(ii)==0:
-		    good=False
-		    break
+            ii, = np.where(weights > 0)
+            if len(ii)==0:
+                good=False
+                break
             xwave.extend(brick[channel].get_wavelength_grid()[ii])
-             #- Average multiple exposures on the same wavelength grid for each channel
-	    xflux.extend(np.average(exp_flux[:,ii], weights=exp_ivar[:,ii], axis=0))
-	    xivar.extend(weights[ii])
+            #- Average multiple exposures on the same wavelength grid for each channel
+            xflux.extend(np.average(exp_flux[:,ii], weights=exp_ivar[:,ii], axis=0))
+            xivar.extend(weights[ii])
 
-	if not good:continue
+        if not good:
+            continue
 
         xwave = np.array(xwave)
         xivar = np.array(xivar)
         xflux = np.array(xflux)
 
         ii = np.argsort(xwave)
-	#flux[i], ivar[i] = resample_flux(wave, xwave[ii], xflux[ii], xivar[ii])
-	fl, iv = resample_flux(wave, xwave[ii], xflux[ii], xivar[ii])
-	flux.append(fl)
-	ivar.append(iv)
-	good_targetids.append(targetid)
-	if not args.print_info is None:
-		s2n=np.median(fl[:-1]*np.sqrt(iv[:-1])/np.sqrt(wave[1:]-wave[:-1]))
-		print targetid,s2n
-		fpinfo.write(str(targetid)+" "+str(s2n)+"\n")
+        #flux[i], ivar[i] = resample_flux(wave, xwave[ii], xflux[ii], xivar[ii])
+        fl, iv = resample_flux(wave, xwave[ii], xflux[ii], xivar[ii])
+        flux.append(fl)
+        ivar.append(iv)
+        good_targetids.append(targetid)
+        if not args.print_info is None:
+            s2n = np.median(fl[:-1]*np.sqrt(iv[:-1])/np.sqrt(wave[1:]-wave[:-1]))
+            if (comm is None) or (comm.rank == 0):
+                print targetid,s2n
+                fpinfo.write(str(targetid)+" "+str(s2n)+"\n")
 
     if not args.print_info is None:
-    	    fpinfo.close()
-	    sys.exit()
+        if (comm is None) or (comm.rank == 0):
+            fpinfo.close()
+        sys.exit()
 
     good_targetids=good_targetids[args.first_spec:]
     flux=np.array(flux[args.first_spec:])
     ivar=np.array(ivar[args.first_spec:])
     nspec=len(good_targetids)
-    log.info("number of good targets = %d"%nspec)
-    if args.nspec is not None and args.nspec<nspec :
-        log.info("Fitting {} of {} targets".format(args.nspec, nspec))
+    if (comm is None) or (comm.rank == 0):
+        log.info("number of good targets = %d"%nspec)
+    if (args.nspec is not None) and (args.nspec < nspec):
+        if (comm is None) or (comm.rank == 0):
+            log.info("Fitting {} of {} targets".format(args.nspec, nspec))
         nspec=args.nspec
         good_targetids=good_targetids[:nspec]
         flux=flux[:nspec]
         ivar=ivar[:nspec]
     else :
-        log.info("Fitting {} targets".format(nspec))
+        if (comm is None) or (comm.rank == 0):
+            log.info("Fitting {} targets".format(nspec))
     
-    log.debug("flux.shape={}".format(flux.shape))
+    if (comm is None) or (comm.rank == 0):
+        log.debug("flux.shape={}".format(flux.shape))
     
+    zf = None
+    if comm is None:
+        # Use multiprocessing built in to RedMonster.
+
+        zf = RedMonsterZfind(wave= wave,flux= flux,ivar=ivar,
+                             objtype=args.objtype,zrange_galaxy= args.zrange_galaxy,
+                             zrange_qso=args.zrange_qso,zrange_star=args.zrange_star,
+                             nproc=args.nproc,npoly=args.npoly)
     
-    zf = RedMonsterZfind(wave= wave,flux= flux,ivar=ivar,
-                         objtype=args.objtype,zrange_galaxy= args.zrange_galaxy,
-                         zrange_qso=args.zrange_qso,zrange_star=args.zrange_star,
-                         nproc=args.nproc,npoly=args.npoly)
-    
-    
+    else:
+        # Use MPI
 
+        # distribute the spectra among processes
+        my_firstspec, my_nspec = dist_uniform(nspec, comm.size, comm.rank)
+        my_specs = slice(my_firstspec, my_firstspec + my_nspec)
+        for p in range(comm.size):
+            if p == comm.rank:
+                if my_nspec > 0:
+                    log.info("process {} fitting spectra {} - {}".format(p, my_firstspec, my_firstspec+my_nspec-1))
+                else:
+                    log.info("process {} idle".format(p))
+                sys.stdout.flush()
+            comm.barrier()
 
+        # do redshift fitting on each process
+        myzf = None
+        if my_nspec > 0:
+            savelevel = os.environ["DESI_LOGLEVEL"]
+            os.environ["DESI_LOGLEVEL"] = "WARNING"
+            myzf = RedMonsterZfind(wave=wave, flux=flux[my_specs,:], ivar=ivar[my_specs,:],
+                             objtype=args.objtype,zrange_galaxy= args.zrange_galaxy,
+                             zrange_qso=args.zrange_qso,zrange_star=args.zrange_star,
+                             nproc=args.nproc,npoly=args.npoly)
+            os.environ["DESI_LOGLEVEL"] = savelevel
 
+        # Combine results into a single ZFindBase object on the root process.
+        # We could do this with a gather, but we are using a small number of
+        # processes, and point-to-point communication is easier for people to
+        # understand.
 
-    # reformat results
-    dtype = list()
+        if comm.rank == 0:
+            zf = ZfindBase(myzf.wave, np.zeros((nspec, myzf.nwave)), np.zeros((nspec, myzf.nwave)), R=None, results=None)
+        
+        for p in range(comm.size):
+            if comm.rank == 0:
+                if p == 0:
+                    # root process copies its own data into output
+                    zf.flux[my_specs] = myzf.flux
+                    zf.ivar[my_specs] = myzf.ivar
+                    zf.model[my_specs] = myzf.model
+                    zf.z[my_specs] = myzf.z
+                    zf.zerr[my_specs] = myzf.zerr
+                    zf.zwarn[my_specs] = myzf.zwarn
+                    zf.type[my_specs] = myzf.type
+                    zf.subtype[my_specs] = myzf.subtype
+                else:
+                    # root process receives from process p and copies
+                    # it into the output.
+                    p_nspec = comm.recv(source=p, tag=0)
+                    # only proceed if the sending process actually
+                    # has some spectra assigned to it.
+                    if p_nspec > 0:
+                        p_firstspec = comm.recv(source=p, tag=1)
+                        p_slice = slice(p_firstspec, p_firstspec+p_nspec)
 
-    dtype = [
-        ('Z',         zf.z.dtype),
-        ('ZERR',      zf.zerr.dtype),
-        ('ZWARN',     zf.zwarn.dtype),
-        ('TYPE',      zf.type.dtype),
-        ('SUBTYPE',   zf.subtype.dtype),    
-    ]
+                        p_flux = comm.recv(source=p, tag=2)
+                        zf.flux[p_slice] = p_flux
 
-    formatted_data  = np.empty(nspec, dtype=dtype)
-    formatted_data['Z']       = zf.z
-    formatted_data['ZERR']    = zf.zerr
-    formatted_data['ZWARN']   = zf.zwarn
-    formatted_data['TYPE']    = zf.type
-    formatted_data['SUBTYPE'] = zf.subtype
-    
+                        p_ivar = comm.recv(source=p, tag=3)
+                        zf.ivar[p_slice] = p_ivar
 
-    # Create a ZfindBase object with formatted results
-    zfi = ZfindBase(None, None, None, results=formatted_data)
-    zfi.nspec = nspec
+                        p_model = comm.recv(source=p, tag=4)
+                        zf.model[p_slice] = p_model
 
-    # QA
-    if (args.qafile is not None) or (args.qafig is not None):
-        log.info("performing skysub QA")
-        # Load
-        qabrick = load_qa_brick(args.qafile)
-        # Run
-        qabrick.run_qa('ZBEST', (zfi,brick))
-        # Write
-        if args.qafile is not None:
-            write_qa_brick(args.qafile, qabrick)
-            log.info("successfully wrote {:s}".format(args.qafile))
-        # Figure(s)
-        if args.qafig is not None:
-            raise IOError("Not yet implemented")
-            qa_plots.brick_zbest(args.qafig, zfi, qabrick)
+                        p_z = comm.recv(source=p, tag=5)
+                        zf.z[p_slice] = p_z
 
+                        p_zerr = comm.recv(source=p, tag=6)
+                        zf.zerr[p_slice] = p_zerr
 
-    #- Write some output
-    if args.outfile is None:
-        args.outfile = io.findfile('zbest', brickname=args.brick)
+                        p_zwarn = comm.recv(source=p, tag=7)
+                        zf.zwarn[p_slice] = p_zwarn
+                        
+                        p_type = comm.recv(source=p, tag=8)
+                        zf.type[p_slice] = p_type
+                        
+                        p_subtype = comm.recv(source=p, tag=9)
+                        zf.subtype[p_slice] = p_subtype
+            else:
+                if p == comm.rank:
+                    # process p sends to root
+                    comm.send(my_nspec, dest=0, tag=0)
+                    if my_nspec > 0:
+                        comm.send(my_firstspec, dest=0, tag=1)
+                        comm.send(myzf.flux, dest=0, tag=2)
+                        comm.send(myzf.ivar, dest=0, tag=3)
+                        comm.send(myzf.model, dest=0, tag=4)
+                        comm.send(myzf.z, dest=0, tag=5)
+                        comm.send(myzf.zerr, dest=0, tag=6)
+                        comm.send(myzf.zwarn, dest=0, tag=7)
+                        comm.send(myzf.type, dest=0, tag=8)
+                        comm.send(myzf.subtype, dest=0, tag=9)
+            comm.barrier()
 
-    log.info("Writing "+args.outfile)
-    #io.write_zbest(args.outfile, args.brick, targetids, zfi, zspec=args.zspec)
-    io.write_zbest(args.outfile, args.brick, good_targetids, zfi, zspec=args.zspec)
+    if (comm is None) or (comm.rank == 0):
+        # The full results exist only on the rank zero process.
 
+        # reformat results
+        dtype = list()
+
+        dtype = [
+            ('Z',         zf.z.dtype),
+            ('ZERR',      zf.zerr.dtype),
+            ('ZWARN',     zf.zwarn.dtype),
+            ('TYPE',      zf.type.dtype),
+            ('SUBTYPE',   zf.subtype.dtype),    
+        ]
+
+        formatted_data  = np.empty(nspec, dtype=dtype)
+        formatted_data['Z']       = zf.z
+        formatted_data['ZERR']    = zf.zerr
+        formatted_data['ZWARN']   = zf.zwarn
+        formatted_data['TYPE']    = zf.type
+        formatted_data['SUBTYPE'] = zf.subtype
+        
+        # Create a ZfindBase object with formatted results
+        zfi = ZfindBase(None, None, None, results=formatted_data)
+        zfi.nspec = nspec
+
+        # QA
+        if (args.qafile is not None) or (args.qafig is not None):
+            log.info("performing skysub QA")
+            # Load
+            qabrick = load_qa_brick(args.qafile)
+            # Run
+            qabrick.run_qa('ZBEST', (zfi,brick))
+            # Write
+            if args.qafile is not None:
+                write_qa_brick(args.qafile, qabrick)
+                log.info("successfully wrote {:s}".format(args.qafile))
+            # Figure(s)
+            if args.qafig is not None:
+                raise IOError("Not yet implemented")
+                qa_plots.brick_zbest(args.qafig, zfi, qabrick)
+
+        #- Write some output
+        if args.outfile is None:
+            args.outfile = io.findfile('zbest', brickname=args.brick)
+
+        log.info("Writing "+args.outfile)
+        #io.write_zbest(args.outfile, args.brick, targetids, zfi, zspec=args.zspec)
+        io.write_zbest(args.outfile, args.brick, good_targetids, zfi, zspec=args.zspec)
+
+    return
 
--- py/desispec/util.py
+++ py/desispec/util.py
@@ -12,6 +12,106 @@ else:
     import multiprocessing as _mp
     default_nproc = max(1, _mp.cpu_count() // 2)
 
+
+# Distribute some number of things among some number
+# of workers as evenly as possible.
+
+def dist_uniform(nwork, workers, id):
+    ntask = 0
+    firsttask = 0
+
+    # if ID is out of range, ignore it
+    if id < workers:
+        if nwork < workers:
+            if id < nwork:
+                ntask = 1
+                firsttask = id
+        else:
+            ntask = int(nwork / workers)
+            leftover = nwork % workers
+            if id < leftover:
+                ntask += 1
+                firsttask = id * ntask
+            else:
+                firsttask = ((ntask + 1) * leftover) + (ntask * (id - leftover))
+    return (firsttask, ntask)
+
+
+# This is effectively the "Painter's Partition Problem".
+
+def distribute_required_groups(A, max_per_group):
+    ngroup = 1
+    total = 0
+    for i in range(A.shape[0]):
+        total += A[i]
+        if total > max_per_group:
+            total = A[i]
+            ngroup += 1
+    return ngroup
+
+def distribute_partition(A, k):
+    low = np.max(A)
+    high = np.sum(A)
+    while low < high:
+        mid = low + int((high - low) / 2)
+        required = distribute_required_groups(A, mid)
+        if required <= k:
+            high = mid
+        else:
+            low = mid + 1
+    return low
+
+def dist_discrete(worksizes, workers, id, pow=1.0):
+    """
+    Distribute indivisible blocks of items between groups.
+
+    Given some contiguous blocks of items which cannot be 
+    subdivided, distribute these blocks to the specified
+    number of groups in a way which minimizes the maximum
+    total items given to any group.  Optionally weight the
+    blocks by a power of their size when computing the
+    distribution.
+
+    Args:
+        worksizes (list): The sizes of the indivisible blocks.
+        workers (int): The number of workers.
+        id (int): The worker ID whose range should be returned.
+        pow (float): The power to use for weighting
+
+    Returns:
+        A tuple.  The first element of the tuple is the first 
+        item assigned to the worker ID, and the second element 
+        is the number of items assigned to the worker.
+    """
+    chunks = np.array(worksizes, dtype=np.int64)
+    weights = np.power(chunks.astype(np.float64), pow)
+    max_per_proc = float(distribute_partition(weights.astype(np.int64), workers))
+
+    target = np.sum(weights) / workers
+
+    dist = []
+
+    off = 0
+    curweight = 0.0
+    proc = 0
+    for cur in range(0, weights.shape[0]):
+        if curweight + weights[cur] > max_per_proc:
+            dist.append( (off, cur-off) )
+            over = curweight - target
+            curweight = weights[cur] + over
+            off = cur
+            proc += 1
+        else:
+            curweight += weights[cur]
+
+    dist.append( (off, weights.shape[0]-off) )
+
+    if len(dist) != workers:
+        raise RuntimeError("Number of distributed groups different than number requested")
+
+    return dist[id]
+
+
 def mask32(mask):
     '''
     Return an input mask as unsigned 32-bit
