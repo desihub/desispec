0e5bf4902e95b24d9f2739f308ec16d3ccaed1e0
Stephen Bailey <stephen.code@gmail.com>
2016-07-12 14:37:16 -0700

    more bootcalib qa coverage

--- py/desispec/test/test_bootcalib.py
+++ py/desispec/test/test_bootcalib.py
@@ -25,6 +25,7 @@ class TestBoot(unittest.TestCase):
         cls.testarc = 'test_arc.fits.gz'
         cls.testflat = 'test_flat.fits.gz'
         cls.testout = 'test_bootcalib_{}.fits'.format(uuid1())
+        self.qafile = 'test-qa-123jkkjiuc4h123h12h3423sadfew.pdf'
         cls.data_unavailable = False
 
         # Grab the data
@@ -55,6 +56,9 @@ class TestBoot(unittest.TestCase):
         #     os.unlink(cls.testflat)
         if os.path.exists(cls.testout):
             os.unlink(cls.testout)
+
+        if os.path.isfile(self.qafile):
+            os.unlink(self.qafile)
 
     def test_fiber_peaks(self):
         if self.data_unavailable:
@@ -158,16 +162,28 @@ class TestBoot(unittest.TestCase):
 
         self.assertLess(all_wv_soln[0]['rms'], 0.25)
 
+    #- desispec.bootcalib.bootcalib may be redundant with
+    #- desispec.scripts.bootcalib.main.  Include tests for both for now.
+
+    #- bootcalib.bootcalib is broken; see https://github.com/desihub/desispec/issues/174
+    @unittest.expectedFailure
+    def test_bootcalib(self):        
+        from desispec.bootcalib import bootcalib
+        from desispec.image import Image
+        arc = fits.getdata(self.testarc)
+        flat = fits.getdata(self.testflat)
+        arcimage = Image(arc, np.ones_like(arc), camera='b0')
+        flatimage = Image(flat, np.ones_like(flat), camera='b0')
+        results = bootcalib(3, flatimage, arcimage)
+
     def test_main(self):
         if self.data_unavailable:
             self.skipTest("Failed to download test data.")
         argstr = [
-            '--fiberflat',
-            self.testflat,
-            '--arcfile',
-            self.testarc,
-            '--outfile',
-            self.testout
+            '--fiberflat', self.testflat,
+            '--arcfile', self.testarc,
+            '--outfile', self.testout,
+            '--qafile', self.qafile,
         ]
         args = bootscript.parse(options=argstr)
         bootscript.main(args)
