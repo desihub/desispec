ca40617b0daa37838db76a955b44b04cba9315a3
Julien Guy <guy@lpnhe.in2p3.fr>
2016-10-13 11:51:07 +0200

    adapt identification of spectrograph arm from camera id for teststand data

--- py/desispec/cosmics.py
+++ py/desispec/cosmics.py
@@ -38,11 +38,19 @@ def reject_cosmic_rays_ala_sdss_single(img,selection,nsig,cfudge,c2fudge) :
     # /project/projectdirs/desi/spectro/redux/alpha-3/calib2d/psf/20150107
     #
     naxis=4
-    if img.camera[0].lower() == 'b':
+
+    
+    band=None
+    if img.camera.lower().find("ccd")>=0 : # ccd naming in teststand is CCDS1B CCDS1R CCDS1Z
+        band=img.camera[-1].lower()
+    else : # ccd naming in simulations is B0 R0 Z0 ...
+        band=img.camera[0].lower()
+    
+    if band == 'b':
         psf=np.array([0.366247,0.391422,0.172965,0.184552])
-    elif img.camera[0].lower() == 'r':
+    elif band == 'r':
         psf=np.array([0.39508155,0.2951822,0.13044542,0.14904523])
-    elif img.camera[0].lower() == 'z':
+    elif band == 'z':
         psf=np.array([0.513852,0.537679,0.297071,0.276298])
     else :
         log.error("do not have psf for camera '%s'"%img.camera)
