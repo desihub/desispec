6d0bf7545da841d821c7d05db0cf2c30f79df68e
Sami Kama <sami.kama.git@gmail.com>
2016-10-25 18:10:51 -0500

    Performance improvements by removing redundant calculations

--- py/desispec/quicklook/procalgs.py
+++ py/desispec/quicklook/procalgs.py
@@ -257,16 +257,23 @@ class BoxcarExtraction(pas.PipelineAlg):
             outfile=kwargs["Outfile"]
         else:
             outfile=None
+        maskFile=None
+        if "MaskFile" in kwargs:
+            maskFile=kwargs['MaskFile']
 
-        return self.run_pa(input_image,psf,wave,boxwidth,nspec,fibers=fibers,fibermap=fibermap,dump=dump,dumpfile=dumpfile)
+        return self.run_pa(input_image,psf
+                           ,wave,boxwidth,nspec,
+                           fibers=fibers,fibermap=fibermap,
+                           dump=dump,dumpfile=dumpfile,maskFile=maskFile)
 
 
-    def run_pa(self, input_image, psf, outwave, boxwidth, nspec,fibers=None, fibermap=None,dump=False,dumpfile=None):
+    def run_pa(self, input_image, psf, outwave, boxwidth, nspec,
+               fibers=None, fibermap=None,dump=False,dumpfile=None,
+               maskFile=None):
         from desispec.boxcar import do_boxcar
         from desispec.frame import Frame as fr
-        
         flux,ivar,Rdata=do_boxcar(input_image, psf, outwave, boxwidth=boxwidth, 
-nspec=nspec)
+                                  nspec=nspec,maskFile=maskFile)
 
         #- write to a frame object
         
