a2d8b79370b047684fa10bc62712ee0d554cc17a
Benjamin Alan Weaver <baweaver@lbl.gov>
2017-03-28 15:03:46 -0700

    use log.warn instead of warnings.warn

--- py/desispec/log.py
+++ py/desispec/log.py
@@ -5,7 +5,7 @@ desispec.log
 This is a transitional dummy wrapper on ``desiutil.log``.
 """
 from __future__ import absolute_import
-from warnings import warn
+# from warnings import warn
 
 from desiutil.log import DEBUG, INFO, WARNING, ERROR, CRITICAL
 from desiutil.log import get_logger as _desiutil_get_logger
@@ -13,6 +13,8 @@ from desiutil.log import get_logger as _desiutil_get_logger
 def get_logger(*args, **kwargs):
     """Transitional dummy wrapper on ``desiutil.log.get_logger()``.
     """
-    warn("desispec.log is deprecated, please use desiutil.log.",
-         DeprecationWarning)
-    return _desiutil_get_logger(*args, **kwargs)
+    # warn("desispec.log is deprecated, please use desiutil.log.",
+    #      DeprecationWarning)
+    log = _desiutil_get_logger(*args, **kwargs)
+    log.warn("desispec.log is deprecated, please use desiutil.log.")
+    return log
