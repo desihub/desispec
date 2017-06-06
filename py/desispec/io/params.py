963cced70d9ddd8707ea6f5aea430996a8dfca91
profxj <xavier@ucolick.org>
2017-06-06 10:56:55 -0700

    now properly cached [I think]

--- py/desispec/io/params.py
+++ py/desispec/io/params.py
@@ -10,18 +10,21 @@ import yaml
 
 from pkg_resources import resource_filename
 
+# CACHE
+params = {}
 
-def read_params(filename=None):
+
+def read_params(filename=None, reload=False):
     """Read parameter data from file
     """
-    global params  # Caching
-
-    # File
-    if filename is None:
-        filename = resource_filename('desispec','/data/params/desispec_param.yml')
-    # Read yaml
-    with open(filename, 'r') as infile:
-        params = yaml.load(infile)
+    global params  # Cache
+    # Init
+    if (len(params) == 0) or (reload is True):
+        if filename is None:
+            filename = resource_filename('desispec','/data/params/desispec_param.yml')
+        # Read yaml
+        with open(filename, 'r') as infile:
+            params = yaml.load(infile)
     # Return
     return params
 
