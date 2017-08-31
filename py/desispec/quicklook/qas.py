2d49c9ee19bec8c6acfef5b263af602f2eb52c3b
Sami Kama <sami.kama.git@gmail.com>
2017-08-30 17:28:49 -0700

    Adding reference checking to QAs

--- py/desispec/quicklook/qas.py
+++ py/desispec/quicklook/qas.py
@@ -11,13 +11,34 @@ class MonitoringAlg:
         self.__inpType__=type(inptype)
         self.name=name
         self.config=config
+        self.__deviation=None
         self.m_log.debug("initializing Monitoring alg {}".format(name))
     def __call__(self,*args,**kwargs):
-        return self.run(*args,**kwargs)
+        res=self.run(*args,**kwargs)
+        res["QA_STATUS"]="UNKNOWN"
+        if "RESULT" in res and "REFERENCE" in self.config:
+            self.__deviation=res["RESULT"] - self.config["REFERENCE"]
+        #check THRESHOLDS given in config and set QA_STATUS keyword
+        # it should be a sorted list of tuples in the form [ ((interval),"Keyword"),((0.,5.)"OK"),((5.10),"Acceptable"),((10.,15),"broken")]
+        # intervals should be non overlapping.
+        # lower bound is inclusive upper bound is exclusive
+        # first matching interval will be used
+        # if no interval contains the deviation, it will be set to "OUTOFBOUNDS"
+        # if THRESHOLDS or REFERENCE are not given in config, QA_STATUS will be set to UNKNOWN 
+        if "THRESHOLDS" in self.config and self.__deviation is not None:
+            res["QA_STATUS"]="OUTOFBOUNDS"
+            d=self.__deviation
+            for l in self.config["THRESHOLDS"]:
+                if d>=l[0][0] and d<l[0][1]:
+                    res["QA_STATUS"]=l[1]
+                    break
+        return res
     def run(self,*argv,**kwargs):
         pass
     def is_compatible(self,Type):
         return isinstance(Type,self.__inpType__)
+    def check_reference():
+        return self.__deviation
     def get_default_config(self):
         """ return a dictionary of 3-tuples,
         field 0 is the name of the parameter
