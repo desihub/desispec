deed03ec5be584061e60c5e0dd9bd24aff6e26de
Sami Kama <sami.kama.git@gmail.com>
2017-09-07 15:06:49 -0700

    Allow multiple thresholds

--- py/desispec/quicklook/qas.py
+++ py/desispec/quicklook/qas.py
@@ -1,6 +1,7 @@
 from desispec.quicklook import qllogger 
 from desispec.quicklook import qlexceptions
-
+from enum import Enum
+class Severity(Enum):
 class MonitoringAlg:
     """ Simple base class for monitoring algorithms """
     def __init__(self,name,inptype,config,logger=None):
@@ -16,22 +17,48 @@ class MonitoringAlg:
     def __call__(self,*args,**kwargs):
         res=self.run(*args,**kwargs)
         res["QA_STATUS"]="UNKNOWN"
+        deviation=None
         if "RESULT" in res and "REFERENCE" in self.config:
-            self.__deviation=res["RESULT"] - self.config["REFERENCE"]
-        #check THRESHOLDS given in config and set QA_STATUS keyword
+            current=resDict["RESULT"]
+            old=self.config["REFERENCE"]
+            currlist=isinstance(current,list)
+            oldlist=isinstance(old,list)
+            if currlist != oldlist: # different types
+                self.m_log.critical("QL {} : REFERENCE({}) and RESULT({}) are of different types!".format(self.name,type(old),type(current)))
+            elif currlist: #both are lists
+                if len(old)==len(current):
+                    self.__deviation=[abs(c-o) for c,o in zip(current,old)]
+                else:
+                    self.m_log.critical("QL {} : REFERENCE({}) and RESULT({}) are of different length!".format(self.name,len(old),len(current)))
+            else: # both are scalars
+                self.__deviation=abs(current-old)
+
+        # check THRESHOLDS given in config and set QA_STATUS keyword
         # it should be a sorted list of tuples in the form [ ((interval),"Keyword"),((0.,5.)"OK"),((5.10),"Acceptable"),((10.,15),"broken")]
+        # for multiple results, thresholds should be a list of list as given above (one threshold list per result)
         # intervals should be non overlapping.
         # lower bound is inclusive upper bound is exclusive
         # first matching interval will be used
         # if no interval contains the deviation, it will be set to "OUTOFBOUNDS"
         # if THRESHOLDS or REFERENCE are not given in config, QA_STATUS will be set to UNKNOWN 
-        if "THRESHOLDS" in self.config and self.__deviation is not None:
-            res["QA_STATUS"]="OUTOFBOUNDS"
-            d=self.__deviation
-            for l in self.config["THRESHOLDS"]:
+        def findThr(d,t):
+            for l in t:
                 if d>=l[0][0] and d<l[0][1]:
-                    res["QA_STATUS"]=l[1]
-                    break
+                    return l[1]
+            return "OUTOFBOUNDS"
+        if self.__deviation and "THRESHOLDS" in self.config:
+            thr=self.config["THRESHOLDS"]
+            res["QA_STATUS"]="ERROR"
+            thrlist=isinstance(thr[0][0][0],list) #multiple threshols for multiple results
+            devlist=isinstance(self.__deviation,list)
+            if devlist!=thrlist:
+                self.m_log.critical("QL {} : dimension of THRESHOLD({}) and RESULTS({}) are incompatible!".format(self.name,len(thr),len(self.__deviation)))
+                return res
+            else:
+                if devlist:
+                    res["QA_STATUS"]=[findThr(d,t) for d,t in zip(self.__deviation,thr)]
+                else:
+                    res["QA_STATUS"]=findThr(self.__deviation,thr)
         return res
     def run(self,*argv,**kwargs):
         pass
