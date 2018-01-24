b50f6546e446c7ae5e069c68794bce25ba27f630
Julien Guy <jguy@lbl.gov>
2018-01-24 11:46:26 -0800

    more generic scores to handle raw,flatfielded,skysub,calibrated fluxes

--- py/desispec/scripts/specscore.py
+++ py/desispec/scripts/specscore.py
@@ -15,12 +15,16 @@ from desiutil.log import get_logger
 from desispec.specscore import compute_frame_scores,append_scores_and_write_frame
 
 def parse(options=None):
-    parser = argparse.ArgumentParser(description="Add a SCORES HDU to a frame or cframe fits file with simple scores on the spectra.")
+    parser = argparse.ArgumentParser(description="Add or modify SCORES HDU to a frame or cframe fits file with simple scores on the spectra.")
     parser.add_argument('-i', '--infile', type = str, default = None, required=True, nargs='*',
                         help = 'list of path to DESI frame fits files')
     parser.add_argument('--overwrite', action="store_true",
                         help = 'The HDU SCORES is overwritten if it already exists in the file')
-    
+    parser.add_argument('--calibrated', action="store_true",
+                        help = 'Are the fluxes calibrated')
+    parser.add_argument('--suffix', type = str, default = None, required=False,
+                        help = 'suffix added to the column name in the SCORES table to describe the level of processing of the spectra in the frame. For instance "RAW","FFLAT","SKYSUB","CALIB"')
+                       
     args = None
     if options is None:
         args = parser.parse_args()
@@ -41,7 +45,7 @@ def main(args) :
         # is it a frame or a cframe ... this depends on the units
 
         
-        new_scores,new_comments=compute_frame_scores(frame)
+        new_scores,new_comments=compute_frame_scores(frame,suffix=args.suffix,calibrated=args.calibrated)
         
         append_scores_and_write_frame(frame,filename,new_scores,new_comments,args.overwrite)
         
