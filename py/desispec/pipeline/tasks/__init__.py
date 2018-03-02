18d5cfd3886000296b870df79b4458585d31ba8b
Julien Guy <jguy@lbl.gov>
2018-03-02 15:43:25 -0800

    enable psf

--- py/desispec/pipeline/tasks/__init__.py
+++ py/desispec/pipeline/tasks/__init__.py
@@ -43,7 +43,7 @@ if base.task_classes is None:
         taskname = tasknamemat.group(1)
 
         # FIXME: remove these lines to test other task types
-        if (taskname != "fibermap") and (taskname != "pix") and  (taskname != "rawdata"):
+        if (taskname != "fibermap") and (taskname != "pix") and  (taskname != "rawdata") and  (taskname != "psf") :
             continue
 
         # import the module
