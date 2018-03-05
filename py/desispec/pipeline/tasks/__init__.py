ff022a947ce227baae05c97222795ad66ac60869
Julien Guy <jguy@lbl.gov>
2018-03-05 10:05:50 -0800

    adding psfnight

--- py/desispec/pipeline/tasks/__init__.py
+++ py/desispec/pipeline/tasks/__init__.py
@@ -43,7 +43,9 @@ if base.task_classes is None:
         taskname = tasknamemat.group(1)
 
         # FIXME: remove these lines to test other task types
-        if (taskname != "fibermap") and (taskname != "pix") and  (taskname != "rawdata") and  (taskname != "psf") :
+        if (taskname != "fibermap") and (taskname != "pix") \
+           and  (taskname != "rawdata") and  (taskname != "psf") \
+           and (taskname != "psfnight") :
             continue
 
         # import the module
