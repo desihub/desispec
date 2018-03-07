a6331e86b44e7186358bafb6519be0eace8bcc2a
Benjamin Alan Weaver <baweaver@lbl.gov>
2018-03-07 16:26:10 -0700

    clean up test_io

--- py/desispec/test/test_io.py
+++ py/desispec/test/test_io.py
@@ -24,12 +24,11 @@ class TestIO(unittest.TestCase):
     def setUpClass(cls):
         """Create unique test filename in a subdirectory.
         """
-        from uuid import uuid1
-        cls.testfile = 'test-{uuid}/test-{uuid}.fits'.format(uuid=uuid1())
-        cls.testyfile = 'test-{uuid}/test-{uuid}.yaml'.format(uuid=uuid1())
-        cls.testbrfile = 'test-{uuid}/test-br-{uuid}.fits'.format(uuid=uuid1())
-        # cls.testDir = os.path.join(os.environ['HOME'],'desi_test_io')
         cls.testDir = tempfile.mkdtemp()
+        cls.testfile = os.path.join(cls.testDir, 'desispec_test_io.fits')
+        cls.testyfile = os.path.join(cls.testDir, 'desispec_test_io.yaml')
+        # cls.testbrfile appears to be unused by this class.
+        cls.testbrfile = os.path.join(cls.testDir, 'desispec_test-br_io.yaml')
         cls.origEnv = {'SPECPROD':None,
             "DESI_SPECTRO_DATA":None,
             "DESI_SPECTRO_REDUX":None}
@@ -61,9 +60,6 @@ class TestIO(unittest.TestCase):
         for testfile in [cls.testfile, cls.testyfile, cls.testbrfile]:
             if os.path.exists(testfile):
                 os.remove(testfile)
-                testpath = os.path.normpath(os.path.dirname(testfile))
-                if testpath != '.':
-                    os.removedirs(testpath)
 
         for e in cls.origEnv:
             if cls.origEnv[e] is None:
@@ -302,7 +298,7 @@ class TestIO(unittest.TestCase):
         self.assertTrue(np.all(fm3['SPECTROID'] == [0,0,0,0,0,1,1,1,1,1]))
 
     # See https://github.com/astropy/astropy/issues/5267
-    @unittest.skipIf(PY3, "Skipping due to known problem with round-tripping in Python 3.")
+    # @unittest.skipIf(PY3, "Skipping due to known problem with round-tripping in Python 3.")
     def test_fibermap_rw(self):
         """Test reading and writing fibermap files.
         """
@@ -556,8 +552,8 @@ class TestIO(unittest.TestCase):
         from ..io.meta import findfile
         from ..io.util import makepath
         # Setup paths
-        os.environ['DESI_SPECTRO_REDUX'] = self.testEnv['DESI_SPECTRO_REDUX']
-        os.environ['SPECPROD'] = self.testEnv['SPECPROD']
+        # os.environ['DESI_SPECTRO_REDUX'] = self.testEnv['DESI_SPECTRO_REDUX']
+        # os.environ['SPECPROD'] = self.testEnv['SPECPROD']
         # Generate a dummy frame file
         x = findfile('frame', camera='b0', night='20150101', expid=123)
         makepath(x)
@@ -574,8 +570,8 @@ class TestIO(unittest.TestCase):
         from ..io.meta import findfile
         from ..io.util import makepath
         # Setup paths
-        os.environ['DESI_SPECTRO_REDUX'] = self.testEnv['DESI_SPECTRO_REDUX']
-        os.environ['SPECPROD'] = self.testEnv['SPECPROD']
+        # os.environ['DESI_SPECTRO_REDUX'] = self.testEnv['DESI_SPECTRO_REDUX']
+        # os.environ['SPECPROD'] = self.testEnv['SPECPROD']
         # Generate a dummy frame file
         for expid, night in zip((123,150), ['20150101', '20150102']):
             x = findfile('cframe', camera='b0', night=night, expid=expid)
@@ -593,8 +589,8 @@ class TestIO(unittest.TestCase):
         from ..io.meta import findfile
         from ..io.util import makepath
         # Setup paths
-        os.environ['DESI_SPECTRO_REDUX'] = self.testEnv['DESI_SPECTRO_REDUX']
-        os.environ['SPECPROD'] = self.testEnv['SPECPROD']
+        # os.environ['DESI_SPECTRO_REDUX'] = self.testEnv['DESI_SPECTRO_REDUX']
+        # os.environ['SPECPROD'] = self.testEnv['SPECPROD']
         # Generate a dummy frame file
         for expid, night in zip((123, 150), ['20150101', '20150102']):
             x = findfile('cframe', camera='b0', night=night, expid=expid)
@@ -618,7 +614,6 @@ class TestIO(unittest.TestCase):
         from ..io.meta import findfile
         from ..io.download import download
         filename = findfile('sky',expid=2,night='20150510',camera='b0',spectrograph=0)
-        print(filename)
         paths = download(filename)
         self.assertEqual(paths[0],filename)
         self.assertTrue(os.path.exists(paths[0]))
