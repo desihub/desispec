13acb33ba56f267f763252b4c8160e72c38d59a9
Sarah Eftekharzadeh <s.eftekharzadeh@gmail.com>
2018-05-03 21:45:04 -0500

    added the placeholder for mags to GENERAL_INFO section

--- py/desispec/quicklook/merger.py
+++ py/desispec/quicklook/merger.py
@@ -74,9 +74,11 @@ def reOrderDict(mergeDict):
              try: dec = [float("%.5f" % m) for m in dec]
              except: dec=None
              
+            #- Add imaging mag by hand to this to facilitate QLF implementation. Should come from calculation/headers... somewhere.
+             imaging_mag=[22.]*500 #- should be nfibers. Get from somewhere or allow None for ra (look except above) in test_ql.py mergeQA test. 
+
              
-             
-             Camera["GENERAL_INFO"]={"SEEING":seeing,"AIRMASS":airmass,"EXPTIME":exptime,"DESISPEC_VERSION":desispec_ver,"RA":ra, "DEC":dec, "SKY_FIBERID":sky_fiberid, "ELG_FIBERID":elg_fiberid ,"LRG_FIBERID":lrg_fiberid, "QSO_FIBERID":qso_fiberid ,"STAR_FIBERID":star_fiberid ,"B_PEAKS":b_peaks ,"R_PEAKS":r_peaks ,"Z_PEAKS":z_peaks}   
+             Camera["GENERAL_INFO"]={"SEEING":seeing,"AIRMASS":airmass,"EXPTIME":exptime,"DESISPEC_VERSION":desispec_ver,"RA":ra, "DEC":dec, "SKY_FIBERID":sky_fiberid, "ELG_FIBERID":elg_fiberid ,"LRG_FIBERID":lrg_fiberid, "QSO_FIBERID":qso_fiberid ,"STAR_FIBERID":star_fiberid ,"B_PEAKS":b_peaks ,"R_PEAKS":r_peaks ,"Z_PEAKS":z_peaks,"IMAGING_MAG": imaging_mag}   
 
 
 class QL_QAMerger:
