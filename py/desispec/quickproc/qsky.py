c64cdfbfaf865d34b8072c3566236cca00184ea9
Julien Guy <jguy@lbl.gov>
2018-06-19 19:07:00 -0700

    iterative sky fit

--- py/desispec/quickproc/qsky.py
+++ py/desispec/quickproc/qsky.py
@@ -3,7 +3,6 @@ import numpy as np
 import scipy.ndimage
 
 from desiutil.log import get_logger
-from desispec.linalg import spline_fit
 from desispec.quickproc.qframe import QFrame
 
 def quick_sky_subtraction(qframe) :
@@ -11,17 +10,13 @@ def quick_sky_subtraction(qframe) :
     t0=time.time()
     log.info("Starting...")
     
-    twave=np.mean(qframe.wave,axis=0)
-    tflux=np.zeros(qframe.flux.shape)
-    tivar=np.zeros(qframe.flux.shape)
-
+    twave=np.linspace(np.min(qframe.wave),np.max(qframe.wave),qframe.wave.shape[1]*2) # oversampling
+    tflux=np.zeros((qframe.flux.shape[0],twave.size))
+    tivar=np.zeros((qframe.flux.shape[0],twave.size))
+    
     if qframe.mask is not None :
         qframe.ivar *= (qframe.mask==0)
-
-    for i in range(qframe.flux.shape[0]) :
-        jj=(qframe.ivar[i]>0)
-        tflux[i]=np.interp(twave,qframe.wave[i,jj],qframe.flux[i,jj])
-    
+        
     if qframe.fibermap is None :
         log.error("Empty fibermap in qframe, cannot know which are the sky fibers!")
         raise RuntimeError("Empty fibermap in qframe, cannot know which are the sky fibers!")
@@ -30,16 +25,21 @@ def quick_sky_subtraction(qframe) :
     if skyfibers.size==0 :
        log.error("No sky fibers!")
        raise RuntimeError("No sky fibers!") 
+    log.info("Sky fibers: {}".format(skyfibers))
     
-    sky  = np.median(tflux[skyfibers],axis=0)
-    for i in range(qframe.flux.shape[0]) :
-        jj=(qframe.flux[i]!=0)
-        qframe.flux[i,jj] -= np.interp(qframe.wave[i,jj],twave,sky)
-    
+    for loop in range(5) : # I need several iterations to remove the effect of the wavelength solution noise
+        
+        for i in skyfibers :
+            jj=(qframe.ivar[i]>0)
+            tflux[i]=np.interp(twave,qframe.wave[i,jj],qframe.flux[i,jj])
+        
+        sky  = np.median(tflux[skyfibers],axis=0)
+        
+        for i in range(qframe.flux.shape[0]) :
+            jj=(qframe.flux[i]!=0)
+            qframe.flux[i,jj] -= np.interp(qframe.wave[i,jj],twave,sky)
+            
+
     t1=time.time()
     log.info(" done in {:3.1f} sec".format(t1-t0))
-
-    #import matplotlib.pyplot as plt
-    #plt.plot(twave,sky)
-    #plt.show()
     
