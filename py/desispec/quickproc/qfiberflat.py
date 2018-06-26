91d0b0654d2de868040a6c76b29e8569debab64d
Julien Guy <jguy@lbl.gov>
2018-06-26 15:00:44 -0700

    compute,write,read and apply desispec.fiberflat.FiberFlat

--- py/desispec/quickproc/qfiberflat.py
+++ py/desispec/quickproc/qfiberflat.py
@@ -5,29 +5,24 @@ import scipy.ndimage
 from desiutil.log import get_logger
 from desispec.linalg import spline_fit
 from desispec.quickproc.qframe import QFrame
+from desispec.fiberflat import FiberFlat
 
-def quick_apply_fiberflat(qframe,qflat) :
+def quick_apply_fiberflat(qframe,fiberflat) :
 
     log = get_logger()
 
-    if len(qframe.fibers) == len(qflat.fibers) and np.sum(qframe.fibers != qflat.fibers)==0  :
-        ii=(qflat.flux!=0)
-        qframe.flux[ii] /= qflat.flux[ii]
-        qframe.ivar[ii] *= qflat.flux[ii]**2
-        qframe.ivar[(qflat.flux<=0)|(qflat.ivar==0)] = 0.
-    else :
-        for j in range(qframe.flux.shape[0]) :
-            k=np.where(qflat.fibers==qframe.fibers[j])[0]
-            if k.size != 1 :
-                log.error("No fiber {} in flat".format(qframe.fibers[j]))
-                raise ValueError("No fiber {} in flat".format(qframe.fibers[j]))
-            k=k[0]
-            ii=(qflat.flux[k]!=0)
-            qframe.flux[j,ii] /= qflat.flux[k,ii]
-            qframe.ivar[j,ii] *= qflat.flux[k,ii]**2
-            qframe.ivar[j,(qflat.flux[k]<=0)|(qflat.ivar[k]==0)] = 0.
+    for j in range(qframe.flux.shape[0]) :
+        k=j#np.where(fiberflat.fibers==qframe.fibers[j])[0]
+        ii=np.where((fiberflat.fiberflat[k]!=0)&(fiberflat.ivar[k]>0)&(fiberflat.mask[k]==0))[0]
+        if ii.size>0 :
+            tmp=np.interp(qframe.wave[j],fiberflat.wave[ii],fiberflat.fiberflat[k,ii],left=0,right=0)
+            qframe.flux[j] *= (tmp>0)/(tmp+(tmp==0)) 
+            qframe.ivar[j] *= tmp**2
+        else :
+            qframe.ivar[j] = 0.
 
-def quick_compute_fiberflat(qframe,niter_meanspec=4,nsig_clipping=3.5,max_flat_uncertainty=0.1,spline_res=10.) :    
+
+def quick_compute_fiberflat(qframe,niter_meanspec=4,nsig_clipping=3.,spline_res_clipping=20.,spline_res_flat=5.) :    
     """
     Fast estimation of fiberflat
     """
@@ -46,75 +41,86 @@ def quick_compute_fiberflat(qframe,niter_meanspec=4,nsig_clipping=3.5,max_flat_u
     for i in range(qframe.flux.shape[0]) :
         jj=(qframe.ivar[i]>0)
         tflux[i]=np.interp(twave,qframe.wave[i,jj],qframe.flux[i,jj])
+        tivar[i]=np.interp(twave,qframe.wave[i,jj],qframe.ivar[i,jj],left=0,right=0)
    
-    # iterative loop to absorb constant term in fiber (should have more parameters)
-    
+    # iterative loop to a absorb constant term in fiber (should have more parameters)
+    a=np.ones(tflux.shape[0])
     for iter in range(niter_meanspec) :
-        mflux=np.median(tflux,axis=0)
+        mflux=np.median(a[:,np.newaxis]*tflux,axis=0)
         for i in range(qframe.flux.shape[0]) :
-            a = np.median(tflux[i,mflux>0]/mflux[mflux>0])
-            if a>0 : tflux[i] /= a
+            a[i] = np.median(tflux[i,mflux>0]/mflux[mflux>0])
+
+    # trivial fiberflat
+    fflat=tflux/(mflux+(mflux==0))
+    fivar=tivar*mflux**2
     
+    mask=np.zeros((fflat.shape), dtype='uint32')
+    chi2=0
     
-    # go back to original grid
-    ok=(mflux>100.)#np.median(mflux)*0.01)
-    lowflux=np.where(np.logical_not(ok))[0]
-    
-    for fiber in range(qframe.flux.shape[0]) :
-        
-        tmp = np.interp(qframe.wave[fiber],twave[ok],mflux[ok],left=0,right=0)
-        good=(tmp!=0)
-        tflux[fiber,good] = qframe.flux[fiber,good]/tmp[good]
-        tivar[fiber,good] = qframe.ivar[fiber,good]*tmp[good]**2
-        good &= (qframe.ivar[fiber]>0)
-        if qframe.mask is not None : 
-            good &= (qframe.mask[fiber]==0)
-        bad  = np.logical_not(good)
-        tflux[fiber,bad] = np.interp(qframe.wave[fiber,bad],qframe.wave[fiber,good],tflux[fiber,good])
-        tivar[fiber,bad] = 0.
-        
+    # spline fit to reject outliers and smooth the flat
+    for fiber in range(fflat.shape[0]) :
         # iterative spline fit
         max_rej_it=5# not more than 5 pixels at a time
         max_bad=1000
         nbad_tot=0
-        
-        for loop in range(10) :
-            
-            good &= (tivar[fiber]>0)
-            
-            # add constrain at first and last wave
-            #tflux[fiber,0]=tflux[fiber,-1]=1.
-            #tivar[fiber,0]=tivar[fiber,-1]=1000.
-            #good[0]=good[-1]=True
-            
-            splineflat = spline_fit(qframe.wave[fiber],qframe.wave[fiber,good],tflux[fiber,good],required_resolution=spline_res,input_ivar=tivar[fiber,good],max_resolution=3*spline_res)
-            chi2 = tivar[fiber]*(tflux[fiber]-splineflat)**2
-            bad=np.where(chi2>nsig_clipping**2)[0]
+        for loop in range(20) :
+            good=(fivar[fiber]>0)
+            splineflat = spline_fit(twave,twave[good],fflat[fiber,good],required_resolution=spline_res_clipping,input_ivar=fivar[fiber,good],max_resolution=3*spline_res_clipping)
+            fchi2 = fivar[fiber]*(fflat[fiber]-splineflat)**2
+            bad=np.where(fchi2>nsig_clipping**2)[0]
             if bad.size>0 :
                 if bad.size>max_rej_it : # not more than 5 pixels at a time
-                    ii=np.argsort(chi2[bad])
+                    ii=np.argsort(fchi2[bad])
                     bad=bad[ii[-max_rej_it:]]
-                tivar[fiber,bad] = 0
+                fivar[fiber,bad] = 0
                 nbad_tot += len(bad)
-                log.warning("iteration {} rejecting {} pixels (tot={}) from fiber {}".format(loop,len(bad),nbad_tot,fiber))
+                #log.warning("iteration {} rejecting {} pixels (tot={}) from fiber {}".format(loop,len(bad),nbad_tot,fiber))
                 if nbad_tot>=max_bad:
-                    tivar[fiber,:]=0
+                    fivar[fiber,:]=0
                     log.warning("1st pass: rejecting fiber {} due to too many (new) bad pixels".format(fiber))
             else :
                 break
-        tflux[fiber] = splineflat
-        valid=np.where((tivar[fiber]>1./max_flat_uncertainty**2))[0]
-        tflux[fiber,:valid[0]]    = 1.
-        tflux[fiber,valid[-1]+1:] = 1.
-        tivar[fiber,tivar[fiber]<1./max_flat_uncertainty**2]=0.
-
-
-    
-
         
+        chi2 += np.sum(fchi2)
+        
+        min_ivar = 0.1*np.median(fivar[fiber])
+        med_flat = np.median(fflat[fiber])
+         
+        good=(fivar[fiber]>0)
+        splineflat = spline_fit(twave,twave[good],fflat[fiber,good],required_resolution=spline_res_flat,input_ivar=fivar[fiber,good],max_resolution=3*spline_res_flat)
+        fflat[fiber] = splineflat # replace by spline
+        
+        ii=np.where(fivar[fiber]>min_ivar)[0]
+        if ii.size<2 :
+            fflat[fiber] = 1
+            fivar[fiber] = 0
+        
+        # set flat in unkown edges to median value of fiber (and ivar to 0)
+        b=ii[0]
+        e=ii[-1]+1
+        fflat[fiber,:b]=med_flat # default
+        fivar[fiber,:b]=0 
+        mask[fiber,:b]=1 # need to change this
+        fflat[fiber,e:]=med_flat # default
+        fivar[fiber,e:]=0 
+        mask[fiber,e:]=1 # need to change this
+        
+        # internal interpolation
+        bad=(fivar[fiber][b:e]<=min_ivar)
+        good=(fivar[fiber][b:e]>min_ivar)
+        fflat[fiber][b:e][bad]=np.interp(twave[b:e][bad],twave[b:e][good],fflat[fiber][b:e][good])
+        
+    
+    ndata=np.sum(fivar>0)
+    if ndata>0 :
+        chi2pdf = chi2/ndata
+    else :
+        chi2pdf = 0
+    
     t1=time.time()
     log.info(" done in {:3.1f} sec".format(t1-t0))
     
+    # return a fiberflat object ...
     
-    return QFrame(qframe.wave, tflux, tivar, mask=None, fibers=qframe.fibers, fibermap=qframe.fibermap)
-
+    return FiberFlat(twave, fflat, fivar, mask, mflux,chi2pdf=chi2pdf)
+    #TO FINISH
