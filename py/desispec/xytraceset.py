c80fb42b727a2722945f7da4552a6ebcc2f5cf10
Julien Guy <jguy@lbl.gov>
2018-07-16 09:29:42 -0700

    add inverted tracesets

--- py/desispec/xytraceset.py
+++ py/desispec/xytraceset.py
@@ -27,6 +27,9 @@ class XYTraceSet(object):
         
         self.x_vs_wave_traceset = TraceSet(xcoef,[wavemin,wavemax])
         self.y_vs_wave_traceset = TraceSet(ycoef,[wavemin,wavemax])
+        self.wave_vs_y_traceset = None
+        self.x_vs_y_traceset    = None
+        
     
     def x_vs_wave(self,fiber,wavelength) :
         return self.x_vs_wave_traceset.eval(fiber,wavelength)
@@ -34,4 +37,12 @@ class XYTraceSet(object):
     def y_vs_wave(self,fiber,wavelength) :
         return self.y_vs_wave_traceset.eval(fiber,wavelength)
     
-        
+    def wave_vs_y(self,fiber,y) :
+        if self.wave_vs_y_traceset is None :
+            self.wave_vs_y_traceset = self.y_vs_wave_traceset.invert()
+        return self.wave_vs_y_traceset.eval(fiber,y)
+    
+    def x_vs_y(self,fiber,y) :
+        FINISH THIS WORK
+        twave = self.wave_vs_y(fiber,y)
+        FINISH THIS WORK
