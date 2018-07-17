3635aa37a1bbd48b37ab16897cdc92eb4ee133a8
Julien Guy <jguy@lbl.gov>
2018-07-17 14:56:12 -0700

    add option to return sky model

--- py/desispec/qproc/qsky.py
+++ py/desispec/qproc/qsky.py
@@ -5,7 +5,7 @@ import scipy.ndimage
 from desiutil.log import get_logger
 from desispec.qproc.qframe import QFrame
 
-def qproc_sky_subtraction(qframe) :
+def qproc_sky_subtraction(qframe,return_skymodel=False) :
     log=get_logger()
     t0=time.time()
     log.info("Starting...")
@@ -13,6 +13,9 @@ def qproc_sky_subtraction(qframe) :
     twave=np.linspace(np.min(qframe.wave),np.max(qframe.wave),qframe.wave.shape[1]*2) # oversampling
     tflux=np.zeros((qframe.flux.shape[0],twave.size))
     tivar=np.zeros((qframe.flux.shape[0],twave.size))
+    
+    if return_skymodel :
+        sky=np.zeros(qframe.flux.shape)
     
     if qframe.mask is not None :
         qframe.ivar *= (qframe.mask==0)
@@ -33,13 +36,22 @@ def qproc_sky_subtraction(qframe) :
             jj=(qframe.ivar[i]>0)
             tflux[i]=np.interp(twave,qframe.wave[i,jj],qframe.flux[i,jj])
         
-        sky  = np.median(tflux[skyfibers],axis=0)
+        ttsky  = np.median(tflux[skyfibers],axis=0)
         
-        for i in range(qframe.flux.shape[0]) :
-            jj=(qframe.flux[i]!=0)
-            qframe.flux[i,jj] -= np.interp(qframe.wave[i,jj],twave,sky)
-            
+        if return_skymodel :
+            for i in range(qframe.flux.shape[0]) :
+                tmp=np.interp(qframe.wave[i],twave,ttsky)
+                jj=(qframe.flux[i]!=0)
+                qframe.flux[i,jj] -= tmp[jj]
+                sky[i] += tmp
+        else :
+            for i in range(qframe.flux.shape[0]) :
+                jj=(qframe.flux[i]!=0)
+                qframe.flux[i,jj] -= np.interp(qframe.wave[i,jj],twave,ttsky)
+        
 
     t1=time.time()
     log.info(" done in {:3.1f} sec".format(t1-t0))
     
+    if return_skymodel :
+        return sky
