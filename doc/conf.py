68a59652f39eea038efc6faa317d8a0d7fff6cdb
Stephen Bailey <stephen.code@gmail.com>
2018-11-06 20:50:24 -0800

    try to work around sphinx problem

--- doc/conf.py
+++ doc/conf.py
@@ -134,7 +134,8 @@ napoleon_include_private_with_doc = True
 # some external dependencies are not met at build time and break the
 # building process.
 autodoc_mock_imports = []
-for missing in ('astropy', 'desimodel', 'desitarget', 'desiutil', 'healpy',
+for missing in ('astropy', 'astropy.modeling',
+                'desimodel', 'desitarget', 'desiutil', 'healpy',
                 'matplotlib', 'numpy', 'redrock', 'scipy', 'speclite', 'specter',
                 'sqlalchemy', 'yaml', 'fitsio', 'numba'):
     try:
