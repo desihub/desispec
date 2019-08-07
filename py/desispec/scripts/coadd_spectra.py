4dbb32b81a6fcb7dc545c7316c980b435551a498
Julien Guy <jguy@lbl.gov>
2019-08-07 11:29:53 -0700

    add cosmic ray rejection

--- py/desispec/scripts/coadd_spectra.py
+++ py/desispec/scripts/coadd_spectra.py
@@ -19,6 +19,7 @@ def parse(options=None):
     parser = argparse.ArgumentParser()
     parser.add_argument("-i","--infile", type=str,  help="input spectra file")
     parser.add_argument("-o","--outfile", type=str,  help="output spectra file")
+    parser.add_argument("--nsig", type=float, default=None, help="nsigma rejection threshold for cosmic rays")
     if options is None:
         args = parser.parse_args()
     else:
@@ -26,14 +27,8 @@ def parse(options=None):
 
     return args
 
-def main(args=None):
-
+def coadd(spectra, cosmics_nsig=0.) :
     log = get_logger()
-
-    if args is None:
-        args = parse()
-
-    spectra = read_spectra(args.infile)
     targets = np.unique(spectra.fibermap["TARGETID"])
     ntarget=targets.size
     log.debug("number of targets= {}".format(ntarget))
@@ -44,11 +39,53 @@ def main(args=None):
         tivar=np.zeros((ntarget,nwave),dtype=spectra.ivar[b].dtype)
         tmask=np.zeros((ntarget,nwave),dtype=spectra.mask[b].dtype)
         trdata=np.zeros((ntarget,spectra.resolution_data[b].shape[1],nwave),dtype=spectra.resolution_data[b].dtype)
+        
         for i,tid in enumerate(targets) :
-            jj=spectra.fibermap["TARGETID"]==tid
+            jj=np.where(spectra.fibermap["TARGETID"]==tid)[0]
+
+
+            if cosmics_nsig > 0 :
+                # interpolate over bad measurements
+                # to be able to compute gradient next
+                # to a bad pixel and identify oulier
+                # many cosmics residuals are on edge
+                # of cosmic ray trace, and so can be
+                # next to a masked flux bin
+                grad=[]
+                gradvar=[]
+                for j in jj :
+                    ttivar = spectra.ivar[b][j]*(spectra.mask[b][j]==0)
+                    good = (ttivar>0)
+                    bad  = (ttivar<=0)
+                    ttflux = spectra.flux[b][j]
+                    ttflux[bad] = np.interp(spectra.wave[b][bad],spectra.wave[b][good],ttflux[good])
+                    ttivar = spectra.ivar[b][j]
+                    ttivar[bad] = np.interp(spectra.wave[b][bad],spectra.wave[b][good],ttivar[good])
+                    ttvar = 1./ttivar
+                    ttflux[1:] = ttflux[1:]-ttflux[:-1]
+                    ttvar[1:]  = ttvar[1:]+ttvar[:-1]
+                    ttflux[0]  = 0
+                    grad.append(ttflux)
+                    gradvar.append(ttvar)
+
             tivar_unmasked= np.sum(spectra.ivar[b][jj],axis=0)
-            tivar[i]=np.sum(spectra.ivar[b][jj]*(spectra.mask[b][jj]==0),axis=0)
-            tflux[i]=np.sum(spectra.ivar[b][jj]*(spectra.mask[b][jj]==0)*spectra.flux[b][jj],axis=0)
+            ivarjj=spectra.ivar[b][jj]*(spectra.mask[b][jj]==0)
+            if len(grad)>0 and cosmics_nsig > 0 :
+                grad=np.array(grad)
+                gradivar=1/np.array(gradvar)
+                nspec=grad.shape[0]
+                meangrad=np.sum(gradivar*grad,axis=0)/np.sum(gradivar)
+                deltagrad=grad-meangrad
+                chi2=np.sum(gradivar*deltagrad**2,axis=0)/(nspec-1)
+                
+                for l in np.where(chi2>cosmics_nsig**2)[0]  :
+                    k=np.argmax(gradivar[:,l]*deltagrad[:,l]**2)
+                    #k=np.argmax(flux[:,j])
+                    log.debug("masking spec {} wave={}".format(k,spectra.wave[b][l]))
+                    ivarjj[k][l]=0.
+            
+            tivar[i]=np.sum(ivarjj,axis=0)
+            tflux[i]=np.sum(ivarjj*spectra.flux[b][jj],axis=0)
             for r in range(spectra.resolution_data[b].shape[1]) :
                 trdata[i,r]=np.sum((spectra.ivar[b][jj]*spectra.resolution_data[b][jj,r]),axis=0) # not sure applying mask is wise here
             bad=(tivar[i]==0)
@@ -103,5 +140,17 @@ def main(args=None):
 
     spectra.fibermap=tfmap
     spectra.scores=None
+
+def main(args=None):
+
+    log = get_logger()
+
+    if args is None:
+        args = parse()
+
+    spectra = read_spectra(args.infile)
+
+    coadd(spectra,cosmics_nsig=args.nsig)
+
     log.debug("writing {} ...".format(args.outfile))
     write_spectra(args.outfile,spectra)
