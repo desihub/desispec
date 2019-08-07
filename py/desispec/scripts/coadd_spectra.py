febf7150e06f05b1e63ac86b4332e1bc23c2aa3c
Julien Guy <jguy@lbl.gov>
2019-08-07 09:57:36 -0700

    first version (without cosmic ray masking, nor averaging of fibermap info, nor scores

--- py/desispec/scripts/coadd_spectra.py
+++ py/desispec/scripts/coadd_spectra.py
@@ -32,5 +32,50 @@ def main(args=None):
         args = parse()
 
     spectra = read_spectra(args.infile)
+    targets = np.unique(spectra.fibermap["TARGETID"])
+    ntarget=targets.size
+    log.debug("number of targets= {}".format(ntarget))
+    for b in spectra._bands :
+        log.debug("coadding band '{}'".format(b))
+        nwave=spectra.wave[b].size
+        tflux=np.zeros((ntarget,nwave),dtype=spectra.flux[b].dtype)
+        tivar=np.zeros((ntarget,nwave),dtype=spectra.ivar[b].dtype)
+        tmask=np.zeros((ntarget,nwave),dtype=spectra.mask[b].dtype)
+        trdata=np.zeros((ntarget,spectra.resolution_data[b].shape[1],nwave),dtype=spectra.resolution_data[b].dtype)
+        for i,tid in enumerate(targets) :
+            jj=spectra.fibermap["TARGETID"]==tid
+            tivar_unmasked= np.sum(spectra.ivar[b][jj],axis=0)
+            tivar[i]=np.sum(spectra.ivar[b][jj]*(spectra.mask[b][jj]==0),axis=0)
+            tflux[i]=np.sum(spectra.ivar[b][jj]*(spectra.mask[b][jj]==0)*spectra.flux[b][jj],axis=0)
+            for r in range(spectra.resolution_data[b].shape[1]) :
+                trdata[i,r]=np.sum((spectra.ivar[b][jj]*spectra.resolution_data[b][jj,r]),axis=0) # not sure applying mask is wise here
+            bad=(tivar[i]==0)
+            if np.sum(bad)>0 :
+                tivar[i][bad] = np.sum(spectra.ivar[b][jj][bad],axis=0) # if all masked, keep original ivar
+                tflux[i][bad] = np.sum(spectra.ivar[b][jj][bad]*spectra.tflux[b][jj][bad],axis=0)
+            ok=(tivar[i]>0)
+            if np.sum(ok)>0 :
+                tflux[i][ok] /= tivar[i][ok]
+            ok=(tivar_unmasked>0)
+            if np.sum(ok)>0 :
+                trdata[i][:,ok] /= tivar_unmasked[ok]
+            tmask[i]      = np.bitwise_and.reduce(spectra.mask[b][jj],axis=0)
+        spectra.flux[b] = tflux
+        spectra.ivar[b] = tivar
+        spectra.mask[b] = tmask
+        spectra.resolution_data[b] = trdata
+    jj=np.zeros(ntarget,dtype=int)
+    for i,tid in enumerate(targets) :
+        jj[i]=np.where(spectra.fibermap["TARGETID"]==tid)[0][0]
+    spectra.fibermap=spectra.fibermap[jj]
     
+    ## now deal with the fibermap
+    #tfmap = spectra.fibermap[:ntarget][:]
+    #for i,tid in enumerate(targets) :
+    #    jj=spectra.fibermap["TARGETID"]==tid
+    #    for k in spectra.fibermap.colnames :
+    #        tfmap[i][k]=spectra.fibermap[jj[0]][k] # take first entry
+    #spectra.fibermap=tfmap
+    spectra.scores=None
+    log.debug("writing {} ...".format(args.outfile))
     write_spectra(args.outfile,spectra)
