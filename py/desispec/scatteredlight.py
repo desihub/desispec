6e7388fcf11a11cc2ed101c18ba769033f767ce4
Julien Guy <jguy@lbl.gov>
2020-01-26 10:04:32 -0800

    improvements

--- py/desispec/scatteredlight.py
+++ py/desispec/scatteredlight.py
@@ -9,7 +9,7 @@ from scipy.signal import fftconvolve
 from desispec.image import Image
 from desiutil.log import get_logger
 
-def model_scattered_light(image,xyset,sigma=50) :
+def model_scattered_light(image,xyset,sigma1=20,sigma2=100,a1=0.8) :
     """
     Args:
       
@@ -35,29 +35,31 @@ def model_scattered_light(image,xyset,sigma=50) :
     mask_out *= (image.mask==0)*image.ivar
              
     log.info("convolving mask_in*image")
-    hw = int(3*sigma)
+    hw = int(3*max(sigma1,sigma2))
     x1d = np.linspace(-hw,hw,2*hw+1)
     x2d = np.tile(x1d,(2*hw+1,1))
     r2  = x2d**2+x2d.T**2
-    kern = np.exp(-0.5*r2/sigma**2) # best is sigma ~=50
-    mod  = fftconvolve(image.pix*mask_in,kern,mode="same")
-    mod *= (mod>0)
+    kern = a1*np.exp(-0.5*r2/sigma1**2)+(1-a1)*np.exp(-0.5*r2/sigma2**2)
+    kern /= np.sum(kern)
+    model  = fftconvolve(image.pix*mask_in,kern,mode="same")
+    model *= (model>0)
+    model2 = model**2  
     n0=image.pix.shape[0]
     n1=image.pix.shape[1]
     yy = np.tile(np.linspace(-1,1,n0),(n1,1)).T
     xx = np.tile(np.linspace(-1,1,n1),(n0,1))
     
     # adjust this model weighted by a polynomial of x and y
-    xp=[0,2,0]
-    yp=[0,0,2]
+    xp=[0,1,2,0,1,0]
+    yp=[0,0,0,1,1,2]
     log.info("compute linear system matrices")
     npar=len(xp)
     B=np.zeros(npar)
     A=np.zeros((npar,npar))
     for i in range(npar) :
-        B[i] = np.sum(mask_out*image.pix*mod*xx**xp[i]*yy**yp[i])
+        B[i] = np.sum(mask_out*image.pix*model*xx**xp[i]*yy**yp[i])        
         for j in range(i+1) :
-            A[i,j] = A[j,i] = np.sum(mask_out*mod**2*(xx**(xp[i]+xp[j])*yy**(yp[i]+yp[j])))
+            A[i,j] = A[j,i] = np.sum(mask_out*model2*(xx**(xp[i]+xp[j])*yy**(yp[i]+yp[j])))
 
     log.info("solve and apply")
     Ai = np.linalg.inv(A)
@@ -65,6 +67,6 @@ def model_scattered_light(image,xyset,sigma=50) :
     log.info("coefficients= {}".format(alpha))
     pmod = np.zeros(mod.shape)
     for i in range(npar) :
-        pmod += alpha[i] * mod * xx**xp[i] * yy**yp[i]
+        pmod += alpha[i] * model * xx**xp[i] * yy**yp[i]
     pmod[pmod<0]=0.
     return pmod
