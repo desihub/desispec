5838d1887fa14cd8d6263406c68b65f229433f23
zkdtc <zkdtckk@gmail.com>
2020-10-21 14:20:34 -0700

    a tested working version

--- py/desispec/preproc.py
+++ py/desispec/preproc.py
@@ -314,7 +314,7 @@ def get_calibration_image(cfinder,keyword,entry,header=None) :
 
     log.info("Using %s %s"%(keyword,filename))
     if keyword == "BIAS" :
-        if header is not None:
+        if header is not None: # Use master bias+dark file if header is set to pass exptime
             log.info('Use bias+dark file: '+filename)
             return read_bias_plus_dark(filename=filename,exptime=int(header['EXPTIME']))
         else:
@@ -784,115 +784,6 @@ def read_bias(filename=None, camera=None, dateobs=None):
     else:
         return fits.getdata(filename, 0)
 
-def bwperim(bw, n=4):
-    """
-    perim = bwperim(bw, n=4)
-    Find the perimeter of objects in binary images.
-    A pixel is part of an object perimeter if its value is one and there
-    is at least one zero-valued pixel in its neighborhood.
-    By default the neighborhood of a pixel is 4 nearest pixels, but
-    if `n` is set to 8 the 8 nearest pixels will be considered.
-    Parameters
-    ----------
-      bw : A black-and-white image
-      n : Connectivity. Must be 4 or 8 (default: 8)
-    Returns
-    -------
-      perim : A boolean image
-
-    From Mahotas: http://nullege.com/codes/search/mahotas.bwperim
-    """
-
-    if n not in (4,8):
-        raise ValueError('mahotas.bwperim: n must be 4 or 8')
-    rows,cols = bw.shape
-
-    # Translate image by one pixel in all directions
-    north = np.zeros((rows,cols))
-    south = np.zeros((rows,cols))
-    west = np.zeros((rows,cols))
-    east = np.zeros((rows,cols))
-
-    north[:-1,:] = bw[1:,:]
-    south[1:,:]  = bw[:-1,:]
-    west[:,:-1]  = bw[:,1:]
-    east[:,1:]   = bw[:,:-1]
-    idx = (north == bw) & \
-          (south == bw) & \
-          (west  == bw) & \
-          (east  == bw)
-    if n == 8:
-        north_east = np.zeros((rows, cols))
-        north_west = np.zeros((rows, cols))
-        south_east = np.zeros((rows, cols))
-        south_west = np.zeros((rows, cols))
-        north_east[:-1, 1:]   = bw[1:, :-1]
-        north_west[:-1, :-1] = bw[1:, 1:]
-        south_east[1:, 1:]     = bw[:-1, :-1]
-        south_west[1:, :-1]   = bw[:-1, 1:]
-        idx &= (north_east == bw) & \
-               (south_east == bw) & \
-               (south_west == bw) & \
-               (north_west == bw)
-    return ~idx * bw
-
-def signed_bwdist(im):
-    '''
-    Find perim and return masked image (signed/reversed)
-    '''    
-    im = -bwdist(bwperim(im))*np.logical_not(im) + bwdist(bwperim(im))*im
-    return im
-
-def bwdist(im):
-    '''
-    Find distance map of image
-    '''
-    from scipy.ndimage.morphology import distance_transform_edt
-    dist_im = distance_transform_edt(1-im)
-    return dist_im
-
-def interp_shape(top, bottom, precision):
-    '''
-    Interpolate between two contours
-
-    Input: top 
-            [X,Y] - Image of top contour (mask)
-           bottom
-            [X,Y] - Image of bottom contour (mask)
-           precision
-             float  - % between the images to interpolate 
-                Ex: num=0.5 - Interpolate the middle image between top and bottom image
-    Output: out
-            [X,Y] - Interpolated image at num (%) between top and bottom
-
-    '''
-    from scipy.interpolate import interpn
-    if precision>1:
-        print("Error: Precision must be between 0 and 1 (float)")
-
-    top = signed_bwdist(top)
-    bottom = signed_bwdist(bottom)
-
-    # row,cols definition
-    r, c = top.shape
-
-    # Reverse % indexing
-    precision = 1+precision
-
-    # rejoin top, bottom into a single array of shape (2, r, c)
-    top_and_bottom = np.stack((top, bottom))
-
-    # create ndgrids 
-    points = (np.r_[0, 2], np.arange(r), np.arange(c))
-    xi = np.rollaxis(np.mgrid[:r, :c], 0, 3).reshape((r*c, 2))
-    xi = np.c_[np.full((r*c),precision), xi]
-
-    # Interpolate for new plane
-    out = interpn(points, top_and_bottom, xi)
-    out = out.reshape((r, c))
-
-    return out
-
 def recover_2d_bias_dark(hdus,exptime,extname):
     log=get_logger()
     #log.info('exptime=',exptime,' extname=',extname)
@@ -965,9 +856,6 @@ def read_bias_plus_dark(filename=None, exptime=0):
             img=image1*precision+image2*(1-precision) #interp_shape(image1,image2,precision)
 
             return img
-
-
-        
 
 def read_pixflat(filename=None, camera=None, dateobs=None):
     '''
