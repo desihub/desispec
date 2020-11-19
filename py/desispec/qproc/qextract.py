80b6492dc6f87a0892b033c1622b85a144dd8bf1
jalasker <jlasker@uchicago.edu>
2020-11-18 17:44:54 -0700

    Fixed typos and replaced bitwise operators with words

--- py/desispec/qproc/qextract.py
+++ py/desispec/qproc/qextract.py
@@ -63,10 +63,10 @@ def qproc_boxcar_extraction(xytraceset, image, fibers=None, width=7, fibermap=No
     ycoef   = xytraceset.y_vs_wave_traceset._coeff 
 
 
-    if (xcoef.shape[0] == 41) & (ycoef.shape[0] == 41):
-        log.warning('This is a traceset from the test slit with only 41 fibers.')\
+    if (xcoef.shape[0] == 41) and (ycoef.shape[0] == 41):
+        log.warning('This is a traceset from the test slit with only 41 fibers.')
         raise NotImplementedError('Nightwatch cannot handle nonstandard numbers of fibers')
-    elif (xcoef.shape[0] !=500) | (ycoef.shape[0] != 500):
+    elif (xcoef.shape[0] !=500) or (ycoef.shape[0] != 500):
         log.warning('This traceset does not have the proper number of fibers in at least one dimension')
         raise NotImplementedError('Nightwatch cannot handle nonstandard numbers of fibers')
 
