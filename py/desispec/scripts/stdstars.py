correct normalization of weights if discarding exposures

py/desispec/scripts/stdstars.py
@@ -224,15 +224,15 @@ def main(args) :
             medflux=np.zeros(nframes)
             for i,frame in enumerate(frames[cam]) :
                 if np.sum(frame.ivar>0) == 0 :
-                    log.error("ivar=0 for all std star spectra in frame {}-{}".format(cam,frame.meta["EXPID"]))
+                    log.error("ivar=0 for all std star spectra in frame {}-{:08d}".format(cam,frame.meta["EXPID"]))
                 else :
                     medflux[i] = np.median(frame.flux[frame.ivar>0])
             log.debug("medflux = {}".format(medflux))
             medflux *= (medflux>0)
-            mmedflux = np.mean(medflux)
-            if mmedflux == 0 :
-                log.error("mean median flux = 0, for all stars in fibers {}".format(list(frames[cam][0].fibermap["FIBER"][starindices])))
-                sys.exit(12)
+            if np.sum(medflux>0)==0 :
+               log.error("mean median flux = 0, for all stars in fibers {}".format(list(frames[cam][0].fibermap["FIBER"][starindices])))
+               sys.exit(12)
+            mmedflux = np.mean(medflux[medflux>0])
             weights=medflux/mmedflux
             log.info("coadding {} exposures in cam {}, w={}".format(nframes,cam,weights))
 