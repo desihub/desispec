9eb7fed48429f6570365d3a8340cce4c3d7af915
Julien Guy <jguy@lbl.gov>
2021-02-24 16:30:55 -0800

    several changes to improve the measurement: selection of fibers, fit of coefficients on rebinned spectra

--- py/desispec/scripts/fibercrosstalk.py
+++ py/desispec/scripts/fibercrosstalk.py
@@ -10,6 +10,7 @@ import numpy as np
 from scipy.signal import fftconvolve
 from astropy.table import Table
 import matplotlib.pyplot as plt
+import scipy.ndimage
 
 from desiutil.log import get_logger
 from desispec.io import read_frame,read_xytraceset
@@ -17,6 +18,8 @@ from desispec.interpolation import resample_flux
 from desispec.calibfinder import findcalibfile
 from desispec.fibercrosstalk import compute_crosstalk_kernels
 from desispec.maskbits import specmask
+
+
 
 def parse(options=None):
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
@@ -50,24 +53,38 @@ def main(args) :
 
     A = None
     B = None
+    out_wave = None
 
     dfiber=np.array([-2,-1,1,2])
+    #dfiber=np.array([-1,1])
+
+    npar=dfiber.size
+    with_cst = True # to marginalize over residual background (should not change much)
+    if with_cst :
+        npar += 1
 
     # one measurement per fiber bundle
     nfiber_per_bundle = 25
-    nbundles=20
+    nbundles = 500//nfiber_per_bundle
+
+    xtalks=[]
+
     previous_psf_filename = None
     for filename in args.infile :
 
         # read a frame and fiber the sky fibers
         frame = read_frame(filename)
 
+        if out_wave is None :
+            dwave=(frame.wave[-1]-frame.wave[0])/40
+            out_wave=np.linspace(frame.wave[0]+dwave/2,frame.wave[-1]-dwave/2,40)
+
         if skymask is None :
             skymask=np.ones(frame.wave.size)
             for line in skylines :
                 skymask[np.abs(frame.wave-line)<2]=0
 
-        skyfibers = np.where(frame.fibermap["OBJTYPE"]=="SKY")[0]
+        skyfibers = np.where((frame.fibermap["OBJTYPE"]=="SKY")&(frame.fibermap["FIBERSTATUS"]==0))[0]
         log.info("{} sky fibers in {}".format(skyfibers.size,filename))
 
         frame.ivar *= ((frame.mask==0)|(frame.mask==specmask.BADFIBER)) # ignore BADFIBER which is a statement on the positioning
@@ -87,17 +104,16 @@ def main(args) :
 
         mwave = np.mean(frame.wave)
 
+
         if A is None :
-            A = np.zeros((nbundles,dfiber.size,dfiber.size,frame.wave.size))
-            B = np.zeros((nbundles,dfiber.size,frame.wave.size))
-            fA = np.zeros((dfiber.size,dfiber.size,frame.wave.size))
-            fB = np.zeros((dfiber.size,frame.wave.size))
+            A = np.zeros((nbundles,npar,npar,out_wave.size))
+            B = np.zeros((nbundles,npar,out_wave.size))
+            fA = np.zeros((npar,npar,out_wave.size))
+            fB = np.zeros((npar,out_wave.size))
             ninput=np.zeros((nbundles,dfiber.size))
-        else :
-            assert(A.shape[3]==frame.wave.size)
 
         for skyfiber in skyfibers :
-            cflux = np.zeros((dfiber.size,frame.wave.size))
+            cflux = np.zeros((dfiber.size,out_wave.size))
             skyfiberbundle=skyfiber//nfiber_per_bundle
 
             nbad=np.sum(frame.ivar[skyfiber]==0)
@@ -113,6 +129,16 @@ def main(args) :
             fA *= 0.
             fB *= 0.
 
+            use_median_filter = False # not needed
+            median_filter_width=30
+            skyfiberflux,skyfiberivar = resample_flux(out_wave,frame.wave,frame.flux[skyfiber],frame.ivar[skyfiber])
+
+            if use_median_filter :
+                good=(skyfiberivar>0)
+                skyfiberflux=np.interp(out_wave,out_wave[good],skyfiberflux[good])
+                skyfiberflux=scipy.ndimage.filters.median_filter(skyfiberflux,median_filter_width,mode='constant')
+
+
             for i,df in enumerate(dfiber) :
                 otherfiber = df+skyfiber
                 if otherfiber<0 : continue
@@ -122,7 +148,7 @@ def main(args) :
 
                 snr=np.sqrt(frame.ivar[otherfiber])*frame.flux[otherfiber]
                 medsnr=np.median(snr)
-                if medsnr>2. :  # need good SNR to model cross talk
+                if medsnr>2 :  # need good SNR to model cross talk
                     should_consider = True # in which case we need all of the contaminants to the sky fiber ...
 
                 nbad=np.sum(snr==0)
@@ -144,46 +170,84 @@ def main(args) :
                 # account for change of wavelength for same y coordinate
                 otherfiber_central_wave = tset.wave_vs_y(otherfiber,central_y)
                 flux = np.interp(frame.wave+(otherfiber_central_wave-skyfiber_central_wave),frame.wave[good],frame.flux[otherfiber][good])
+                if use_median_filter :
+                    flux = scipy.ndimage.filters.median_filter(flux,median_filter_width,mode='constant')
                 kern=kernels[np.abs(df)]
-                cflux[i] = fftconvolve(flux,kern,mode="same")
-                fB[i] = frame.ivar[skyfiber]*cflux[i]*frame.flux[skyfiber]
+                tmp = fftconvolve(flux,kern,mode="same")
+                cflux[i] = resample_flux(out_wave,frame.wave,tmp)
+
+                fB[i] = skyfiberivar*cflux[i]*skyfiberflux
                 for j in range(i+1) :
-                    fA[i,j] = frame.ivar[skyfiber]*cflux[i]*cflux[j]
+                    fA[i,j] = skyfiberivar*cflux[i]*cflux[j]
+
+
 
             if should_consider and ( not must_exclude ) :
+
+
+
+                scflux=np.sum(cflux,axis=0)
+                mscflux=np.sum(skyfiberivar*scflux)/np.sum(skyfiberivar)
+                if mscflux < 500 :
+                    continue
+
+
+
+                if with_cst :
+                    i=dfiber.size
+                    fA[i,i] = skyfiberivar # constant term
+                    fB[i] = skyfiberivar*skyfiberflux
+                    for j in range(i) :
+                        fA[i,j] = skyfiberivar*cflux[j]
+
+                # just stack all wavelength to get 1 number for this fiber
+                scflux=np.sum(cflux[np.abs(dfiber)==1],axis=0)
+                a=np.sum(skyfiberivar*scflux**2)
+                b=np.sum(skyfiberivar*scflux*skyfiberflux)
+                xtalk=b/a
+                err=1./np.sqrt(a)
+                msky=np.sum(skyfiberivar*skyfiberflux)/np.sum(skyfiberivar)
+                ra=frame.fibermap["TARGET_RA"][skyfiber]
+                dec=frame.fibermap["TARGET_DEC"][skyfiber]
+                #log.info("skyfiber = {}, xtalk = {:4.3f} +- {:4.3f}, ra = {:5.4f} , dec = {:5.4f}, sky fiber flux= {:4.3f}, cont= {:4.3f}".format(skyfiber,xtalk,err,ra,dec,msky,mcont))
+
+                if np.abs(xtalk)>0.01 and np.abs(xtalk)/err>5 :
+                    log.warning("discard skyfiber = {}, xtalk = {:4.3f} +- {:4.3f}, ra = {:5.4f} , dec = {:5.4f}, sky fiber flux= {:4.3f}, cont= {:4.3f}".format(skyfiber,xtalk,err,ra,dec,msky,mscflux))
+                    continue
+
+                if err<0.01/5. :
+                    xtalks.append(xtalk)
+
                 for i in range(dfiber.size) :
                     ninput[skyfiberbundle,i] += int(np.sum(fB[i])!=0) # to monitor
                 B[skyfiberbundle] += fB
                 A[skyfiberbundle] += fA
 
+
     for bundle in range(nbundles) :
-        for i in range(dfiber.size) :
+        for i in range(npar) :
             for j in range(i) :
                 A[bundle,j,i] = A[bundle,i,j]
 
     # now solve
-    tmp = np.zeros((nbundles,dfiber.size,frame.wave.size))
-    tmp_ivar = np.zeros((nbundles,dfiber.size,frame.wave.size))
+    crosstalk = np.zeros((nbundles,dfiber.size,out_wave.size))
+    crosstalk_ivar = np.zeros((nbundles,dfiber.size,out_wave.size))
     for bundle in range(nbundles) :
-        for j in range(frame.wave.size) :
+        for j in range(out_wave.size) :
             try:
                 Ai=np.linalg.inv(A[bundle,:,:,j])
-                tmp[bundle,:,j]=Ai.dot(B[bundle,:,j])
-                tmp_ivar[bundle,:,j]=1./np.diag(Ai)
+                if with_cst :
+                    crosstalk[bundle,:,j]=Ai.dot(B[bundle,:,j])[:-1] # last coefficient is constant
+                    crosstalk_ivar[bundle,:,j]=1./np.diag(Ai)[:-1]
+                else :
+                    crosstalk[bundle,:,j]=Ai.dot(B[bundle,:,j])
+                    crosstalk_ivar[bundle,:,j]=1./np.diag(Ai)
+
             except np.linalg.LinAlgError as e :
                 pass
-    # rebin
-    wave=np.linspace(frame.wave[0],frame.wave[-1],40)
-    crosstalk      = np.zeros((nbundles,dfiber.size,wave.size))
-    crosstalk_ivar = np.zeros((nbundles,dfiber.size,wave.size))
-
-    for bundle in range(nbundles) :
-        for i,df in enumerate(dfiber) :
-            crosstalk[bundle,i],crosstalk_ivar[bundle,i]=resample_flux(wave,frame.wave,tmp[bundle,i],tmp_ivar[bundle,i])
-
 
     table=Table()
-    table["WAVELENGTH"]=wave
+    table["WAVELENGTH"]=out_wave
     for bundle in range(nbundles) :
         for i,df in enumerate(dfiber) :
             key="CROSSTALK-B{:02d}-F{:+d}".format(bundle,df)
@@ -191,7 +255,7 @@ def main(args) :
             key="CROSSTALKIVAR-B{:02d}-F{:+d}".format(bundle,df)
             table[key]=crosstalk_ivar[bundle,i]
             key="NINPUT-B{:02d}-F{:+d}".format(bundle,df)
-            table[key]=np.repeat(ninput[bundle,i],wave.size)
+            table[key]=np.repeat(ninput[bundle,i],out_wave.size)
 
     table.write(args.outfile,overwrite=True)
     log.info("wrote {}".format(args.outfile))
