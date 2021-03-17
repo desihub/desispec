834850638b4f218d50d09ab27f2177d9428e7af8
Julien Guy <jguy@lbl.gov>
2021-03-16 19:01:24 -0700

    new approach. saturate half light radius

--- py/desispec/fiberfluxcorr.py
+++ py/desispec/fiberfluxcorr.py
@@ -95,6 +95,13 @@ def psf_to_fiber_flux_correction(fibermap,exposure_seeing_fwhm=1.1) :
     extended_sources = ~point_sources
     half_light_radius_arcsec = fibermap["SHAPE_R"]
 
+    # saturate half_light_radius_arcsec at 2 arcsec
+    # larger values would have extrapolated fiberfrac
+    # when in fact the ratio of fiberfrac for difference seeing
+    # or fiebr angular size are similar
+    max_radius=2.0
+    half_light_radius_arcsec[half_light_radius_arcsec>max_radius]=max_radius
+
     # for current seeing, fiber plate scale , fiber size ...
     current_fiber_frac_point_source  = fa.value("POINT",sigmas_um,offsets_um)
     current_fiber_frac = current_fiber_frac_point_source.copy()
@@ -102,8 +109,8 @@ def psf_to_fiber_flux_correction(fibermap,exposure_seeing_fwhm=1.1) :
     current_fiber_frac[extended_sources] = fa.value("DISK",sigmas_um[extended_sources],offsets_um[extended_sources],half_light_radius_arcsec[extended_sources])
 
     # for "nominal" fiber size of 1.5 arcsec, and seeing of 1.
-    isotropic_platescale = 107/1.5 # um/arcsec
-    sigmas_um   = 1.0/2.35 * isotropic_platescale*np.ones(nfibers) # um
+    nominal_isotropic_platescale = 107/1.5 # um/arcsec
+    sigmas_um   = 1.0/2.35 * nominal_isotropic_platescale*np.ones(nfibers) # um
     offsets_um  = np.zeros(nfibers) # um , no offset
 
     nominal_fiber_frac_point_source = fa.value("POINT",sigmas_um,offsets_um)
@@ -120,29 +127,28 @@ def psf_to_fiber_flux_correction(fibermap,exposure_seeing_fwhm=1.1) :
     if np.sum(to_saturate)>0 :
         imaging_fiber_frac[selection][to_saturate] = imaging_fiber_frac_for_point_source # max is point source value
 
+    """
+    uncalibrated flux     ~= current_fiber_frac * total_flux
+    psf calibrated flux   ~= current_fiber_frac * total_flux / current_fiber_frac_point_source
+    fiber flux            = nominal_fiber_frac * total_flux
 
-    # uncalibrated flux     ~= current_fiber_frac * total_flux
-    # psf calibrated flux   ~= current_fiber_frac * total_flux / current_fiber_frac_point_source
-    # fiber flux            = nominal_fiber_frac * total_flux
-    #
-    # to the multiplicative factor to apply to the current psf calibrated flux is:
-    #
-    # correction_current = (fiber flux)/(psf calibrated flux) = nominal_fiber_frac / current_fiber_frac * current_fiber_frac_point_source
-    #
-    # if we were to observe in nominal conditions, we would have obtained :
-    #
-    # correction_nominal = (fiber flux, nominal conditions)/(psf calibrated flux, nominal_conditions) = nominal_fiber_frac_point_source
-    #
-    # now we have a better measurement of this correction in nominal conditions from the imaging
-    #
-    # correction_nominal_imaging = FIBER_FLUX_R/FLUX_R = imaging_fiber_frac
-    #
-    # we return the current correction calibrated with the imaging :
-    #
-    # correction_final = correction_current * ( correction_nominal_imaging / correction_nominal)
-    # correction_final =  nominal_fiber_frac / current_fiber_frac * current_fiber_frac_point_source / nominal_fiber_frac_point_source * imaging_fiber_frac
-    #
-    corr=np.ones(nfibers)
-    ok=(current_fiber_frac>0)&(nominal_fiber_frac>0)&(current_fiber_frac_point_source>0)&(nominal_fiber_frac_point_source>0)
-    corr[ok]=(nominal_fiber_frac[ok]/current_fiber_frac[ok])*(current_fiber_frac_point_source[ok]/nominal_fiber_frac_point_source[ok])
-    return imaging_fiber_frac * corr
+    the multiplicative factor to apply to the current psf calibrated flux is:
+    correction_current = (fiber flux)/(psf calibrated flux) = nominal_fiber_frac / current_fiber_frac * current_fiber_frac_point_source
+
+    multiply by normalization between the fast fiber acceptance computation (using moffat with beta=3.5) and the one
+    done for the imaging surveys assuming a Gaussian seeing of sigma=1/2.35 arcsec and a fiber of 1.5 arcsec diameter
+
+    """
+
+
+    # compute normalization between the fast fiber acceptance computation and the one
+    # done the imaging surveys assuming a Gaussian seeing of sigma=1/2.35 arcsec and a fiber of 1.5 arcsec diameter
+    scale = 0.789 / np.mean(nominal_fiber_frac_point_source)
+    nominal_fiber_frac *= scale
+
+    corr = current_fiber_frac_point_source
+    ok = (current_fiber_frac>0.01)
+    corr[ok]  *= (nominal_fiber_frac[ok] / current_fiber_frac[ok])
+    corr[~ok] *= 0.
+
+    return corr
