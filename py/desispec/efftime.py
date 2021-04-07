d09975b647aa8086bf9c62451eb8769af4fd31af
Julien Guy <jguy@lbl.gov>
2021-04-07 14:33:58 -0700

    add FIBERFAC,FIBERFAC_ELG,FIBEFRAC_BGS and use this to compute the effective times

--- py/desispec/efftime.py
+++ py/desispec/efftime.py
@@ -12,16 +12,13 @@ import numpy as np
 # AR https://desi.lbl.gov/trac/wiki/SurveyOps/SurveySpeed
 # AR 2021-03-05: GFA values are now computed for a fiber diameter of 1.52"
 def compute_efftime(table,
-                    ffrac_psf_nom=0.58176816, # FRACFLUX_NOMINAL_POINTSOURCE in Aaron's table
-                    ffrac_elg_nom=0.42423388, # FRACFLUX_NOMINAL_ELG
-                    ffrac_bgs_nom=0.19544029, # FRACFLUX_NOMINAL_BGS
                     kterm=0.114, # KERM
                     ebv_r_coeff=2.165,
                     fiber_diameter_arcsec=1.52):
     """Computes the effective exposure times using transparency and fiber acceptance from the GFAs
     offline analysis, and the sky magnitudes from the spectroscopy.
     Uses the formulae described in https://desi.lbl.gov/trac/wiki/SurveyOps/SurveySpeed
-    for the dark,bright and backup programs.
+    for the dark, bright and backup programs.
 
     Args:
       table: astropy.table.Table with columns
@@ -30,9 +27,9 @@ def compute_efftime(table,
          - EBV E(B-V) from SFD map
          - TRANSPARENCY_GFA transparency (number between 0 and 1, ~1 for photometric nights irrespectively of AIRMASS)
          - AIRMASS (airmass >=1)
-         - FIBER_FRACFLUX_GFA fraction of point source flux in fiber aperture (number between 0 and 1)
-         - FIBER_FRACFLUX_ELG_GFA fraction of flux for a typical ELG in fiber aperture (number between 0 and 1)
-         - FIBER_FRACFLUX_BGS_GFA fraction of flux for a typical BGS target in fiber aperture (number between 0 and 1)
+         - FIBERFAC_GFA PMGSTARS forced photometry amount of light in 1.52 asec diameter aperture normalized to nominal, assuming a point source profile
+         - FIBERFAC_ELG_GFA PMGSTARS forced photometry amount of light in 1.52 asec diameter aperture normalized to nominal, assuming an ELG-like profile (r_half = 0.45 asec exponential)
+         - FIBERFAC_BGS_GFA PMGSTARS forced photometry amount of light in 1.52 asec diameter aperture normalized to nominal, assuming a BGS-like profile (r_half = 1.5 asec de Vaucouleurs)
     """
 
     exptime  = table["EXPTIME"]
@@ -41,15 +38,13 @@ def compute_efftime(table,
     ebv      = table["EBV"]
     transparency = table["TRANSPARENCY_GFA"]
     airmass      = table["AIRMASS"]
-    ffrac_psf    = table["FIBER_FRACFLUX_GFA"] #
-    ffrac_elg    = table["FIBER_FRACFLUX_ELG_GFA"] #
-    ffrac_bgs    = table["FIBER_FRACFLUX_BGS_GFA"] #
+    fiberfac_psf = table["FIBERFAC_GFA"]
+    fiberfac_elg = table["FIBERFAC_ELG_GFA"]
+    fiberfac_bgs = table["FIBERFAC_BGS_GFA"]
 
     exptime_nom = 1000.0  # AR seconds
     sky_nom = 3.73  # AR nMgy/arcsec**2
-    fflux_bright_nom = (
-        15.8  # AR nMgy/arcsec**2 (r=19.5 mag for de Vaucouleurs rhalf=1.5" BGS)
-    )
+    fflux_bright_nom = 15.8  # AR nMgy/arcsec**2 (r=19.5 mag for de Vaucouleurs rhalf=1.5" BGS)
     fflux_backup_nom = 27.5  # AR nMgy/arcsec**2 (r=18.9 mag star)
     # AR airmass term
     airfac = 10.0 ** (kterm * (airmass - 1.0) / 2.5)
@@ -58,15 +53,13 @@ def compute_efftime(table,
     # AR sky readnoise
     sky_rdn = 0.932  # AR nMgy/arcsec**2
 
-    fiber_area_arcsec2 = np.pi*(fiber_diameter_arcsec/2.)**2
-
     # AR "limit" fiber flux
-    fflux_bright = (
-        ffrac_bgs * transparency / airfac * fflux_bright_nom / fiber_area_arcsec2
-    )
-    fflux_backup = (
-        ffrac_psf * transparency / airfac * fflux_backup_nom / fiber_area_arcsec2
-    )
+    fiber_area_arcsec2 = np.pi*(fiber_diameter_arcsec/2)**2
+
+    # we divide by fiber_area_arcsec2  because the sky flux has been divided by the fiber_area_arcsec2
+    fflux_bright = fflux_bright_nom * fiberfac_bgs / airfac / ebvfac / fiber_area_arcsec2
+    fflux_backup = fflux_backup_nom * fiberfac_psf / airfac / ebvfac / fiber_area_arcsec2
+
     # AR effective sky
     effsky_dark = (sky + sky_rdn * exptime_nom / exptime) / (1.0 + sky_rdn / sky_nom)
     effsky_bright = (sky + sky_rdn * exptime_nom / exptime + fflux_bright) / (
@@ -78,19 +71,19 @@ def compute_efftime(table,
     # AR effective exposure time
     efftime_dark = (
         exptime
-        * (ffrac_elg / ffrac_elg_nom * transparency / airfac) ** 2
+        * (fiberfac_elg / airfac) ** 2
         * (sky_nom / effsky_dark)
         / ebvfac ** 2
     )
     efftime_bright = (
         exptime
-        * (ffrac_bgs / ffrac_bgs_nom * transparency / airfac) ** 2
+        * (fiberfac_bgs / airfac) ** 2
         * (sky_nom / effsky_bright)
         / ebvfac ** 2
     )
     efftime_backup = (
         exptime
-        * (ffrac_psf / ffrac_psf_nom * transparency / airfac) ** 2
+        * (fiberfac_psf / airfac) ** 2
         * (sky_nom / effsky_backup)
         / ebvfac ** 2
     )
