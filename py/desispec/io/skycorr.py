34c81acf2d4e2534f9d8b18adb9cc7512c1d4f93
Julien Guy <jguy@lbl.gov>
2021-08-25 15:16:32 -0700

    add SkyCorrPCA I/O

--- py/desispec/io/skycorr.py
+++ py/desispec/io/skycorr.py
@@ -9,6 +9,7 @@ import os
 import time
 from astropy.io import fits
 from desiutil.log import get_logger
+import numpy as np
 
 from . import iotime
 
@@ -29,7 +30,7 @@ def write_skycorr(outfile, skycorr):
 
     hx = fits.HDUList()
     hdr['EXTNAME'] = 'WAVELENGTH'
-    hx.append( fits.PrimaryHDU(skycorr.wave.astype('f4'), header=hdr) )
+    hx.append( fits.PrimaryHDU(skycorr.wave.astype('f8'), header=hdr) )
     hx.append( fits.ImageHDU(skycorr.dwave.astype('f4'), name='DWAVE') )
     hx.append( fits.ImageHDU(skycorr.dlsf.astype('f4'), name='DLSF') )
     hx['DWAVE'].header['BUNIT'] = 'Angstrom'
@@ -68,3 +69,88 @@ def read_skycorr(filename) :
     log.info(iotime.format('read', filename, duration))
 
     return SkyCorr(wave, dwave, dlsf, header=hdr)
+
+def write_skycorr_pca(outfile, skycorrpca):
+    """Write sky model.
+
+    Args:
+        outfile : filename or (night, expid, camera) tuple
+        skycorrpca : SkyCorrPCA object
+    """
+    from .util import fitsheader, makepath
+
+    log = get_logger()
+    outfile = makepath(outfile, 'skycorr')
+
+    #- Convert header to fits.Header if needed
+    hdr = fitsheader(skycorrpca.header)
+
+    hx = fits.HDUList()
+    hdr['EXTNAME'] = 'DWAVE_MEAN'
+    hx.append( fits.PrimaryHDU(skycorrpca.dwave_mean.astype('f4'), header=hdr) )
+    for i in range(skycorrpca.dwave_eigenvectors.shape[0]) :
+        hx.append( fits.ImageHDU(skycorrpca.dwave_eigenvectors[i].astype('f4'), name='DWAVE_EIG{}'.format(i+1)))
+    hx.append( fits.ImageHDU(skycorrpca.dwave_eigenvalues.astype('f8'), name='DWAVE_EIGENVALS'))
+
+    hx.append( fits.ImageHDU(skycorrpca.dlsf_mean.astype('f4'), name='DLSF_MEAN'))
+    for i in range(skycorrpca.dlsf_eigenvectors.shape[0]) :
+        hx.append( fits.ImageHDU(skycorrpca.dlsf_eigenvectors[i].astype('f4'), name='DLSF_EIG{}'.format(i+1)))
+    hx.append( fits.ImageHDU(skycorrpca.dlsf_eigenvalues.astype('f8'), name='DLSF_EIGENVALS'))
+
+    hx.append( fits.ImageHDU(skycorrpca.wave.astype('f8'), name='WAVELENGTH'))
+
+    t0 = time.time()
+    hx.writeto(outfile+'.tmp', overwrite=True, checksum=True)
+    os.rename(outfile+'.tmp', outfile)
+    duration = time.time() - t0
+    log.info(iotime.format('write', outfile, duration))
+    return outfile
+
+def read_skycorr_pca(filename) :
+    """Read sky correction pca file and return SkyCorrPCA object.
+    """
+    from .meta import findfile
+    from .util import native_endian
+    from ..skycorr import SkyCorrPCA
+
+    log = get_logger()
+    t0 = time.time()
+    fx = fits.open(filename, memmap=False, uint=True)
+
+    hdr = fx[0].header
+    wave  = native_endian(fx["WAVELENGTH"].data.astype('f8'))
+    dwave_mean = native_endian(fx["DWAVE_MEAN"].data.astype('f8'))
+    dwave_eigenvectors = []
+    for i in range(1,12):
+        key="DWAVE_EIG{}".format(i)
+        if key in fx :
+            dwave_eigenvectors.append(fx[key].data.astype('f8'))
+        else:
+            break
+    dwave_eigenvectors = np.array(dwave_eigenvectors)
+    dwave_eigenvalues = native_endian(fx["DWAVE_EIGENVALS"].data.astype('f8'))
+
+    dlsf_mean = native_endian(fx["DLSF_MEAN"].data.astype('f8'))
+    dlsf_eigenvectors = []
+    for i in range(1,12):
+        key="DLSF_EIG{}".format(i)
+        if key in fx :
+            dlsf_eigenvectors.append(fx[key].data.astype('f8'))
+        else:
+            break
+    dlsf_eigenvectors = np.array(dlsf_eigenvectors)
+
+    dlsf_eigenvalues = native_endian(fx["DLSF_EIGENVALS"].data.astype('f8'))
+
+    fx.close()
+    duration = time.time() - t0
+    log.info(iotime.format('read', filename, duration))
+
+    return SkyCorrPCA(dwave_mean=dwave_mean,
+                      dwave_eigenvectors=dwave_eigenvectors,
+                      dwave_eigenvalues=dwave_eigenvalues,
+                      dlsf_mean=dlsf_mean,
+                      dlsf_eigenvectors=dlsf_eigenvectors,
+                      dlsf_eigenvalues=dlsf_eigenvalues,
+                      wave=wave,
+                      header=hdr)
