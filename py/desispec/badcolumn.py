f75a9ab6972627924814306f48d9c33a01aa3802
Julien Guy <jguy@lbl.gov>
2021-09-01 09:31:40 -0700

    convert VALUE in electrons to electrons/sec

--- py/desispec/badcolumn.py
+++ py/desispec/badcolumn.py
@@ -11,7 +11,8 @@ from desiutil.log import get_logger
 from desispec.maskbits import specmask,fibermask
 
 def flux_bias_function(delta_x) :
-    """Multiply the excess counts in a bad column by this function to determine the bias on the extracted flux of a spectral trace situated at a distance dx (in pixels) from the bad CCD column
+    """Multiply the excess counts in a bad column by this function to determine the bias on the extracted flux of a spectral trace situated at a distance dx (in pixels) from the bad CCD column. This has been derived empirically on the r and z cameras (with inverse variance cross-dispersion profile weighted extractions).
+    https://github.com/desihub/desispec/pull/1371#issuecomment-904961936
 
     Args:
 
@@ -31,15 +32,28 @@ def flux_bias_function(delta_x) :
     else :
         return val
 
-def compute_badcolumn_specmask(frame,xyset,badcolumns_table,threshold_value=2) :
+def compute_badcolumn_specmask(frame,xyset,badcolumns_table,threshold_value=0.005) :
     """
-    returns mask numpy array of same shape as frame.flux if some spectral values are masked
-    or None if nothing is masked
+    fills a mask for spectral values affected by a bad CCD column.
+
+    Args:
+
+     frame: desispec.frame.Frame instance
+     xyset: desispec.xytraceset.XYTraceSet instance (valid for the frame)
+     badcolumns_table: astropy.table.Table with columns "COLUMN" (CCD column index)
+                       and "ELEC_PER_SEC", value in CCD column, in electron / sec
+     threshold_value: threshold for masking spectral pixels, in electron / sec
+
+    Returns:
+
+     mask numpy array of same shape as frame.flux, or None if nothing is masked
     """
 
     if len(badcolumns_table)==0 : return None
 
     log = get_logger()
+
+    log.debug("Threshold value = {:.4f} electrons/sec".format(threshold_value))
 
     if frame.mask is None :
         mask=np.zeros(frame.flux.shape,dtype='uint32')
@@ -52,9 +66,9 @@ def compute_badcolumn_specmask(frame,xyset,badcolumns_table,threshold_value=2) :
     for fiber in range(frame.flux.shape[0]) :
         fiber_x[fiber] = xyset.x_vs_wave(fiber,frame.wave)
 
-    for column_x,column_val in zip(badcolumns_table["COLUMN"],badcolumns_table["VALUE"]) :
+    for column_x,column_val in zip(badcolumns_table["COLUMN"],badcolumns_table["ELEC_PER_SEC"]) :
         dx = fiber_x - column_x
-        log.info("Processing col at x={} val={}".format(column_x,column_val))
+        log.info("Processing col at x={} val={:.4f}".format(column_x,column_val))
         bias = column_val*flux_bias_function(dx)
         mask[np.abs(bias)>=threshold_value] |= specmask.BADCOLUMN
 
@@ -62,6 +76,7 @@ def compute_badcolumn_specmask(frame,xyset,badcolumns_table,threshold_value=2) :
     nvals=np.sum(nvalsperfiber)
     nfibers=np.sum(nvalsperfiber>0)
     log.info("Masked {} flux values from {} fibers".format(nvals,nfibers))
+
 
     return mask
 
@@ -72,9 +87,9 @@ def compute_badcolumn_fibermask(frame_mask,threshold_specfrac=0.4) :
     fiber_mask[badfibers] |= fibermask.BADCOLUMN
     return fiber_mask
 
-def add_badcolumn_mask(frame,xyset,badcolumns_table,threshold_value=2,threshold_specfrac=0.4) :
+def add_badcolumn_mask(frame,xyset,badcolumns_table,threshold_value=0.005,threshold_specfrac=0.4) :
 
-    mask = compute_badcolumn_specmask(frame=frame,xyset=xyset,badcolumns_table=badcolumns_table)
+    mask = compute_badcolumn_specmask(frame=frame,xyset=xyset,badcolumns_table=badcolumns_table,threshold_value=threshold_value)
     if mask is None :
         return # don't do anything
 
@@ -83,5 +98,5 @@ def add_badcolumn_mask(frame,xyset,badcolumns_table,threshold_value=2,threshold_
     else :
         frame.mask = mask
 
-    fiber_mask = compute_badcolumn_fibermask(frame.mask)
+    fiber_mask = compute_badcolumn_fibermask(frame.mask,threshold_specfrac=threshold_specfrac)
     frame.fibermap["FIBERSTATUS"] |= fiber_mask
