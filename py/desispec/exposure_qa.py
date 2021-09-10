976d469f1c6960e64111cc0b493fa4ca3c6d64ff
Julien Guy <jguy@lbl.gov>
2021-09-10 16:16:38 -0700

    bug fix and correct rms

--- py/desispec/exposure_qa.py
+++ py/desispec/exposure_qa.py
@@ -266,7 +266,7 @@ def compute_exposure_qa(night, expid, specprod_dir):
                     dflux,ivar=resample_flux(wave,cframe.wave,cframe.flux[goodfibers_indices[i]],cframe.ivar[goodfibers_indices[i]])
                     scale[i] = np.sum(dflux*mflux)/np.sum(mflux**2)
                 log.debug("scale={}".format(scale))
-                calib_rms=np.sqrt(np.mean((scale-1)**2))
+                calib_rms=np.sqrt(np.mean((scale-1)**2))*np.sqrt(ngood/(ngood-1.))
                 petalqa_table["STARRMS"][petal]=calib_rms
 
                 if ngood >= qa_params["min_number_of_good_stdstars_per_petal"] :
@@ -274,7 +274,7 @@ def compute_exposure_qa(night, expid, specprod_dir):
                 else : # stricter requirement because only few stars
                     max_rms = qa_params["max_rms_of_rflux_ratio_of_stdstars_if_few_stars"]
 
-                if calib_rms>qa_params["max_rms_of_rflux_ratio_of_stdstars"] :
+                if calib_rms>max_rms :
                     log.warning("petal #{} has std stars calib rms={:3.2f}>{:3.2f}".format(petal,calib_rms,qa_params["max_rms_of_rflux_ratio_of_stdstars"]))
                     fiberqa_table['QAFIBERSTATUS'][entries] |= fibermask.mask("BADPETALSTDSTAR")
                 else :
