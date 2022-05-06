13c71c007293efdfb4d56fbf627f94e40b13c284
Rongpu Zhou <rongpu@users.noreply.github.com>
2022-05-06 15:51:20 -0700

    cleanup validredshifts.py

--- py/desispec/validredshifts.py
+++ py/desispec/validredshifts.py
@@ -9,10 +9,25 @@ import fitsio
 
 
 def get_good_fiberstatus(cat, isqso=False):
+    '''
+    Validate the fiber status for a redrock catalog.
+
+    Args:
+        cat: redrock catalog (e.g., in astropy table format)
+
+    Options:
+        isqso: bool (default False), if True, use the specific QSO criteria
+
+    Returns:
+        good_fiberstatus: boolean array
+    '''
+
     if not isqso:
         good_fiberstatus = cat['COADD_FIBERSTATUS']==0
     else:
-        good_fiberstatus = (cat['COADD_FIBERSTATUS']==0) | (cat['COADD_FIBERSTATUS']==8388608) | (cat['COADD_FIBERSTATUS']==16777216)
+        # good_fiberstatus = (cat['COADD_FIBERSTATUS']==0) | (cat['COADD_FIBERSTATUS']==8388608) | (cat['COADD_FIBERSTATUS']==16777216)
+        from desispec.maskbits import fibermask
+        good_fiberstatus = (cat['COADD_FIBERSTATUS']==0) | (cat['COADD_FIBERSTATUS']==fibermask['BADAMPR']) | (cat['COADD_FIBERSTATUS']==fibermask['BADAMPZ'])
     good_fiberstatus &= cat['ZWARN'] & 2**9 == 0  # NO DATA flag
     return good_fiberstatus
 
@@ -22,22 +37,26 @@ def validate(redrock_path, fiberstatus_cut=True, return_target_columns=False, ex
     Validate the redshift quality with tracer-dependent criteria for redrock+afterburner results.
 
     Args:
-       redrock_path: str, path of redrock FITS file
+        redrock_path: str, path of redrock FITS file
 
     Options:
-       fiberstatus_cut: bool (default True), if True, impose requirements on COADD_FIBERSTATUS and ZWARN
-       return_target_columns: bool (default False), if True, include columns that indicate if the object belongs to each class of DESI targets
-       extra_columns: list of str (default None), additional columns to include in the output
-       emline_path: str (default None), specify the location of the emline file; by default the emline file is in the same directory as the redrock file
-       ignore_emline: bool (default False), if True, ignore the emline file and do not validate the ELG redshift
+        fiberstatus_cut: bool (default True), if True, impose requirements on COADD_FIBERSTATUS and ZWARN
+        return_target_columns: bool (default False), if True, include columns that indicate if the object belongs to each class of DESI targets
+        extra_columns: list of str (default None), additional columns to include in the output
+        emline_path: str (default None), specify the location of the emline file; by default the emline file is in the same directory as the redrock file
+        ignore_emline: bool (default False), if True, ignore the emline file and do not validate the ELG redshift
+
+    Returns:
+        cat: astropy table with basic columns such as TARGETID and boolean columns (e.g., GOOD_BGS)
+             that indicate if each object meets the redshift quality criteria of specific tracers
     '''
 
-    output_columns = ['good_bgs', 'good_lrg', 'good_elg', 'good_qso']
+    output_columns = ['GOOD_BGS', 'GOOD_LRG', 'GOOD_ELG', 'GOOD_QSO']
     if return_target_columns:
         output_columns += ['LRG', 'ELG', 'QSO', 'ELG_LOP', 'ELG_HIP', 'ELG_VLO', 'BGS_ANY', 'BGS_FAINT', 'BGS_BRIGHT']
 
     if ignore_emline:
-        output_columns.remove('good_elg')
+        output_columns.remove('GOOD_ELG')
 
     if extra_columns is None:
         extra_columns = ['TARGETID', 'Z', 'ZWARN', 'COADD_FIBERSTATUS']
@@ -87,47 +106,68 @@ def validate(redrock_path, fiberstatus_cut=True, return_target_columns=False, ex
         cat = hstack([tmp_redshifts, tmp_fibermap, tmp_qso_mgii, tmp_qso_qn], join_type='inner')
 
     if return_target_columns:
-        # Bitmask definitions: https://github.com/desihub/desitarget/blob/master/py/desitarget/data/targetmask.yaml
-        cat['LRG'] = cat['DESI_TARGET'] & 2**0 > 0
-        cat['ELG'] = cat['DESI_TARGET'] & 2**1 > 0
-        cat['QSO'] = cat['DESI_TARGET'] & 2**2 > 0
-        cat['ELG_LOP'] = cat['DESI_TARGET'] & 2**5 > 0
-        cat['ELG_HIP'] = cat['DESI_TARGET'] & 2**6 > 0
-        cat['ELG_VLO'] = cat['DESI_TARGET'] & 2**7 > 0
-        cat['BGS_ANY'] = cat['DESI_TARGET'] & 2**60 > 0
-        cat['BGS_FAINT'] = cat['BGS_TARGET'] & 2**0 > 0
-        cat['BGS_BRIGHT'] = cat['BGS_TARGET'] & 2**1 > 0
-        # # or use desitarget.targetmask:
-        # from desitarget.targetmask import desi_mask, bgs_mask
-        # for name in ["LRG", "ELG", "QSO", "ELG_LOP", "ELG_VLO", "BGS_ANY", "BGS_FAINT", "BGS_BRIGHT"]:
-        #     if name in ["BGS_FAINT", "BGS_BRIGHT"]:
-        #         cat[name] = cat["BGS_TARGET"] & bgs_mask[name] > 0
-        #     else:
-        #         cat[name] = cat["DESI_TARGET"] & desi_mask[name] > 0
+        from desitarget.targetmask import desi_mask, bgs_mask
+        for name in ['LRG', 'ELG', 'QSO', 'ELG_LOP', 'ELG_HIP', 'ELG_VLO', 'BGS_ANY', 'BGS_FAINT', 'BGS_BRIGHT']:
+            if name in ['BGS_FAINT', 'BGS_BRIGHT']:
+                cat[name] = cat['BGS_TARGET'] & bgs_mask[name] > 0
+            else:
+                cat[name] = cat['DESI_TARGET'] & desi_mask[name] > 0
+        # # Bitmask definitions: https://github.com/desihub/desitarget/blob/master/py/desitarget/data/targetmask.yaml
+        # cat['LRG'] = cat['DESI_TARGET'] & 2**0 > 0
+        # cat['ELG'] = cat['DESI_TARGET'] & 2**1 > 0
+        # cat['QSO'] = cat['DESI_TARGET'] & 2**2 > 0
+        # cat['ELG_LOP'] = cat['DESI_TARGET'] & 2**5 > 0
+        # cat['ELG_HIP'] = cat['DESI_TARGET'] & 2**6 > 0
+        # cat['ELG_VLO'] = cat['DESI_TARGET'] & 2**7 > 0
+        # cat['BGS_ANY'] = cat['DESI_TARGET'] & 2**60 > 0
+        # cat['BGS_FAINT'] = cat['BGS_TARGET'] & 2**0 > 0
+        # cat['BGS_BRIGHT'] = cat['BGS_TARGET'] & 2**1 > 0
 
-    ############################ Apply criteria for good redshifts ############################
+    cat = actually_validate(cat, fiberstatus_cut=fiberstatus_cut, ignore_emline=ignore_emline)
+    cat = cat[output_columns]
+
+    return cat
+
+
+def actually_validate(cat, fiberstatus_cut=True, ignore_emline=False):
+    '''
+    Apply redshift quality criteria
+
+    Args:
+        cat: astropy table with the necessary columns for redshift quality determination
+
+    Options:
+        fiberstatus_cut: bool (default True), if True, impose requirements on COADD_FIBERSTATUS and ZWARN
+        return_target_columns: bool (default False), if True, include columns that indicate if the object belongs to each class of DESI targets
+        extra_columns: list of str (default None), additional columns to include in the output
+        emline_path: str (default None), specify the location of the emline file; by default the emline file is in the same directory as the redrock file
+        ignore_emline: bool (default False), if True, ignore the emline file and do not validate the ELG redshift
+
+    Returns:
+        cat: astropy table with boolean columns (e.g., GOOD_BGS) added
+    '''
 
     # BGS
-    cat['good_bgs'] = cat['ZWARN']==0
-    cat['good_bgs'] &= cat['DELTACHI2']>40
+    cat['GOOD_BGS'] = cat['ZWARN']==0
+    cat['GOOD_BGS'] &= cat['DELTACHI2']>40
     if fiberstatus_cut:
-        cat['good_bgs'] &= get_good_fiberstatus(cat)
+        cat['GOOD_BGS'] &= get_good_fiberstatus(cat)
 
     # LRG
-    cat['good_lrg'] = cat['ZWARN']==0
-    cat['good_lrg'] &= cat['Z']<1.5
-    cat['good_lrg'] &= cat['DELTACHI2']>15
+    cat['GOOD_LRG'] = cat['ZWARN']==0
+    cat['GOOD_LRG'] &= cat['Z']<1.5
+    cat['GOOD_LRG'] &= cat['DELTACHI2']>15
     if fiberstatus_cut:
-        cat['good_lrg'] &= get_good_fiberstatus(cat)
+        cat['GOOD_LRG'] &= get_good_fiberstatus(cat)
 
     # ELG
     if not ignore_emline:
         with warnings.catch_warnings():
-            warnings.simplefilter("ignore")
-            cat['good_elg'] = (cat['OII_FLUX']>0) & (cat['OII_FLUX_IVAR']>0)
-            cat['good_elg'] &= np.log10(cat['OII_FLUX'] * np.sqrt(cat['OII_FLUX_IVAR'])) > 0.9 - 0.2 * np.log10(cat['DELTACHI2'])
+            warnings.simplefilter('ignore')
+            cat['GOOD_ELG'] = (cat['OII_FLUX']>0) & (cat['OII_FLUX_IVAR']>0)
+            cat['GOOD_ELG'] &= np.log10(cat['OII_FLUX'] * np.sqrt(cat['OII_FLUX_IVAR'])) > 0.9 - 0.2 * np.log10(cat['DELTACHI2'])
         if fiberstatus_cut:
-            cat['good_elg'] &= get_good_fiberstatus(cat)
+            cat['GOOD_ELG'] &= get_good_fiberstatus(cat)
 
     # QSO - adopted from the code from Edmond
     # https://github.com/echaussidon/LSS/blob/8ca53f4c38cfa29722ee6958687e188cc894ed2b/py/LSS/qso_cat_utils.py#L282
@@ -143,10 +183,8 @@ def validate(redrock_path, fiberstatus_cut=True, return_target_columns=False, ex
     # Correct bump at z~3.7
     sel_pb_redshift = (((cat['Z'] > 3.65) & (cat['Z'] < 3.9)) | ((cat['Z'] > 5.15) & (cat['Z'] < 5.35))) & ((cat['C_LYA'] < 0.95) | (cat['C_CIV'] < 0.95))
     cat['QSO_MASKBITS'][sel_pb_redshift] = 0
-    cat['good_qso'] = cat['QSO_MASKBITS']>0
+    cat['GOOD_QSO'] = cat['QSO_MASKBITS']>0
     if fiberstatus_cut:
-        cat['good_qso'] &= get_good_fiberstatus(cat, isqso=True)
-
-    cat = cat[output_columns]
+        cat['GOOD_QSO'] &= get_good_fiberstatus(cat, isqso=True)
 
     return cat
