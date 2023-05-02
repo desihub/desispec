c1a25cce8affd98013b1046d0b93b56cdf62c1e7
akremin <akremin@lbl.gov>
2023-05-02 15:19:19 -0700

    Better logging of change of state decision in daily_proc

--- py/desispec/scripts/daily_processing.py
+++ py/desispec/scripts/daily_processing.py
@@ -349,7 +349,7 @@ def daily_processing_manager(specprod=None, exp_table_path=None, proc_table_path
 
             # if this is a new tile/obstype, proceed with submitting all of the jobs for the previous tile
             if lasttype is not None and ((curtype != lasttype) or (curtile != lasttile)):
-                print("\nNow done with previous tile or obstype. "
+                print("\nData for previous tile or obstype is complete. Running joint fits. "
                       + f"{curtype=}, {lasttype=}, {curtile=}, {lasttile=}\n")
                 old_iid = internal_id
                 # If done with science exposures for a tile and use_tilenight==True, use
