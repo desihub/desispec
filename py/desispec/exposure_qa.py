8d3b3ffd19f8f26870ea9b4a3760dce12c66a5e3
Stephen Bailey <stephen.code@gmail.com>
2024-01-03 13:23:57 -0800

    fix resources.files typo

--- py/desispec/exposure_qa.py
+++ py/desispec/exposure_qa.py
@@ -26,7 +26,7 @@ def get_qa_params() :
     """
     global _qa_params
     if _qa_params is None :
-        param_filename = resources('desispec').joinpath('data/qa/qa-params.yaml')
+        param_filename = resources.files('desispec').joinpath('data/qa/qa-params.yaml')
         with open(param_filename) as f:
             _qa_params = yaml.safe_load(f)
     return _qa_params
