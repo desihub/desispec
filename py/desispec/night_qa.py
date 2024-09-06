81e76e0c2a1923e7fa36fb37346dd7a5dbf0aa1f
anand_raichoor <araichoor@lbl.gov>
2024-09-06 16:12:48 -0700

    create_petalnz_pdf(): additional check requiring tileid to be in tiles-survey.ecsv

--- py/desispec/night_qa.py
+++ py/desispec/night_qa.py
@@ -1679,6 +1679,21 @@ def create_petalnz_pdf(
             )
         )
         tileids, surveys = tileids[sel], surveys[sel]
+    # AR additional check if the tile is in tiles-{survey}.ecsv, as get_tilecov() needs that
+    # AR https://github.com/desihub/desispec/issues/2357
+    sel = np.ones(len(tileids), dtype=bool)
+    for survey in np.unique(surveys):
+        fn = os.path.join(os.getenv("DESI_SURVEYOPS"), "ops", "tiles-{}.ecsv".format(survey))
+        t = Table.read(fn)
+        reject = (surveys == survey) & (~np.in1d(tileids, t["TILEID"]))
+        if reject.sum() > 0:
+            log.warning(
+                "ignoring tiles={} which have survey={} but are not present in {}".format(
+                    ",".join(tileids[reject].astype(str)), survey, fn,
+                )
+            )
+            sel[reject] = False
+    tileids, surveys = tileids[sel], surveys[sel]
     # AR for main tiles, restrict to tiles with EFFTIME > MINTFRAC * GOALTIME
     # AR we also read the tile-qa*fits header below in the loop,
     # AR    but it is simpler/safer to do separate this first loop
