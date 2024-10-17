5963e9f47485227ecddd9f874b653487f0a35dd2
Stephen Bailey <stephen.code@gmail.com>
2024-10-17 15:09:28 -0700

    correct tearDownClass, simplify

--- py/desispec/test/test_example.py
+++ py/desispec/test/test_example.py
@@ -11,33 +11,22 @@ class TestExample(unittest.TestCase):
     # setUpClass runs once at the start before any tests
     @classmethod
     def setUpClass(cls):
-        cls.blat = 1
+        pass
 
-    # setUpClass runs once at the end after every test
-    # e.g. to remove files created by setUpClass
+    # tearDownClass runs once at the end after every test
     @classmethod
-    def setUpClass(cls):
-        cls.blat = 1
+    def tearDownClass(cls):
+        pass
 
-    # setUp runs before every test, e.g. to reset state
+    # setUp runs before every test
     def setUp(self):
-        self.foo = 2
+        pass
 
-    # setUp runs after every test, e.g. to reset state
+    # setUp runs after every test
     def tearDown(self):
         pass
 
     def test_blat(self):
-        self.assertEqual(self.blat, 1)
+        self.assertEqual(1, 1)
 
-    def test_foo(self):
-        self.assertEqual(self.foo, 2)
-        self.foo *= 2
-        self.assertEqual(self.foo, 4)
-
-    def test_foo_again(self):
-        #- even though test_foo changed self.foo, self.setUp() should reset it
-        self.assertEqual(self.foo, 2)
-        self.foo *= 2
-        self.assertEqual(self.foo, 4)
 
