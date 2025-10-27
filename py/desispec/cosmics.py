fb6be4ec3f9ff70e49beb229a45ff7791cddbb46
John Banovetz <jdbanovetz@lbl.gov>
2025-10-27 09:58:04 -0700

    Changed the parameters for the ITL red senors to ones measured by eye

--- py/desispec/cosmics.py
+++ py/desispec/cosmics.py
@@ -385,7 +385,7 @@ def reject_cosmic_rays_ala_sdss(img,nsig=6.,cfudge=3.,c2fudge=0.5,niter=6,dilate
         if 'CCDTYPE' in img.meta:
             if 'ITL' in img.meta['CCDTYPE']:
                 log.info('This red dectector has an ITL chip, using a different PSF gradient')
-                psf_gradients=np.array([0.758221,0.771945,0.570183,0.592199])
+                psf_gradients=np.array([0.62,0.64,0.44,0.46])
         else:
         # desi_compute_psf_gradients -i $DESI_SPECTRO_CALIB/spec/sp2/psf-r2-20190308.fits
             psf_gradients=np.array([0.819245,0.847529,0.617514,0.656629])
